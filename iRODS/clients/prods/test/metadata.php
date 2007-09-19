2b7003204010144b5c1160f964e50bfaf5bf9f41
sifang <sifang@sdsc.edu>
2007-09-19 22:33:05 +0000

    Move code from RODS to iRODS
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@66 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/prods/test/metadata.php
+++ iRODS/clients/prods/test/metadata.php
@@ -9,6 +9,7 @@ $target_dir='/tempZone/home/rods/pictures';
 try {
 $start_time=microtime(true);
 
+/*
 $dir=new ProdsDir($account, $target_dir);
 
 $metas=$dir->rmMetaByID(11053);
@@ -19,6 +20,14 @@ $metas=$dir->getMeta();
 
 
 var_dump($metas);
+*/
+
+echo "--- getting all possible metadata names --- <br/>\n";
+$que=new ProdsQuery($account);
+$metanames=$que->getMetadataNamesForAllFiles();
+var_dump($metanames);
+$metanames2=$que->getMetadataNamesForAllDirs();
+var_dump($metanames2);
 
 $end_time=microtime(true);
 $exec_time=$end_time-$start_time;
