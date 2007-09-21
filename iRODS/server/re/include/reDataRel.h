b3e05e416e187e480ec0cf8f09232d9e59065cdb
kremenek <kremenek@sdsc.edu>
2007-09-21 18:50:07 +0000

    GetDataObjChksumsTimeStampsFromAVU works with the new CVS tree
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@81 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/re/include/reDataRel.h
+++ iRODS/server/re/include/reDataRel.h
@@ -1,2 +1,10 @@
 /* reDataRel.h */
 
+typedef struct
+{
+  char attribute[NAME_MAX+1];
+  char value[NAME_MAX+1];
+  char units[NAME_MAX+1];
+} UserDefinedMetadata_t;
+
+int iGetDataObjChksumsTimeStampsFromAVU (collInp_t* outCollInp, UserDefinedMetadata_t* aAVUarray, int* countUserDefinedMetadata, char* strOut, ruleExecInfo_t *rei);
--- iRODS/server/re/src/reDataRel.c
+++ iRODS/server/re/src/reDataRel.c
@@ -1,31 +1,193 @@
 /*** Copyright (c), The Regents of the University of California            ***
  *** For more information please refer to files in the COPYRIGHT directory ***/
+
 #include <stdarg.h>
 #include "apiHeaderAll.h"
 #include "objStat.h"
 #include "reDataObjOpr.h"
 #include "reGlobalsExtern.h"
 #include "reDataRel.h"
-   
-#ifdef GJK3
-extern struct UserDefinedMetadata
-{
-  char attribute[NAME_MAX+1];
-  char value[NAME_MAX+1];
-  char units[NAME_MAX+1];
-};
 
-extern struct UserDefinedMetadata UAVArray[1024];
-extern int countUserDefinedMetadata;
-#endif
 
 /*
- * Gets pipe separated metadata AVUs for a data object.
+ * Get all Dates of Performed Checksum Operations from metadata AVUs for a given iRods data object.
  * 
  */
-int msiGetDataObjChksumsTimeStampsFromAVU(msParam_t* queryParam, msParam_t* genQueryOutParam, ruleExecInfo_t *rei)
-     // int msiExecStrCondQuery(msParam_t* queryParam, msParam_t* genQueryOutParam, ruleExecInfo_t *rei)
-     // int msiGetDataObjChksumsTimeStampsFromAVU(msParam_t *inpParam, msParam_t *outParam, ruleExecInfo_t *rei)
+
+int
+msiGetDataObjChksumsTimeStampsFromAVU(msParam_t *inpParam1, msParam_t *outParam1, ruleExecInfo_t *rei)
+{
+ rsComm_t *rsComm; 
+ collInp_t collInpCache, *ptrInpColl;
+ int iErr=0, iI=0, i=0;
+ UserDefinedMetadata_t aAVUarray[1024];
+ int countUserDefinedMetadata;
+ char strOut[MAX_NAME_LEN*MAX_NAME_LEN], strTmp[1024];
+
+ /* For testing mode when used with irule --test */
+ RE_TEST_MACRO ("RE_TEST_MACRO, begin of msiGetDataObjChksumsTimeStampsFromAVU") ;
+ 
+   printf ("GJK-P P.1.0.0. in msiGetDataObjChksumsTimeStampsFromAVU(), GJK msiGetDataObjChksumsTimeStampsFromAVU: GJK Calling msiGetDataObjChksumsTimeStampsFromAVU\n");  
+   
+   rsComm = rei->rsComm;
+
+   /* parse inpParam11 */
+   rei->status = parseMspForCollInp (inpParam1, &collInpCache, &ptrInpColl, 0);
+   
+   if (rei->status < 0) {
+     rodsLog (LOG_ERROR, "msiGetDataObjChksumsTimeStampsFromAVU(),  input inpParam1 error. status = %d", rei->status);
+     return (rei->status);
+   }
+
+   iErr = iGetDataObjChksumsTimeStampsFromAVU (ptrInpColl, aAVUarray, &countUserDefinedMetadata, strOut, rei);
+
+   for (iI = 0; iI < countUserDefinedMetadata; iI++) {
+     
+     snprintf (strTmp, MAX_NAME_LEN,"|%s|%s|%s|\n",
+	       aAVUarray[iI].attribute, aAVUarray[iI].value, aAVUarray[iI].units);
+     strncat(strOut, strTmp, MAX_NAME_LEN);
+   }
+   
+   //   sprintf(strOut, "#1\n#2\n\n#3 lines gjk\n");
+   i = fillStrInMsParam (outParam1, strOut);  // MsParam.c parse  addformatedtrsing to bytes WriteBytesBuff printMsParam.c
+   // fillBuffInParam
+   
+   
+   return (iErr);
+}
+
+/* ****************************************************************************************** */
+int iGetDataObjChksumsTimeStampsFromAVU (collInp_t* ptrInpColl, UserDefinedMetadata_t* aAVUarray, int* countUserDefinedMetadata, char* strOut, ruleExecInfo_t *rei)
+{
+  char *chrPtr1 = NULL, strAbsPath[MAX_NAME_LEN], v1[1024], v2[1024], v3[1024], strDirName[MAX_NAME_LEN], strFileName[MAX_NAME_LEN], *condVal[10], attrName[256]="MD5checkSumDataStamp";;
+  rsComm_t *rsComm;
+  genQueryInp_t genQueryInp;
+  int i1a[10], i1b[10], i2a[10], iI=0, iErr=0, printCount=0;
+  bytesBuf_t *byteBuf;
+  genQueryOut_t *genQueryOut;
+
+
+  /* Make sure input is a collection */
+   chrPtr1 = strrchr(ptrInpColl->collName, '/');
+   printf ("GJK-P P.1.0.1. in iGetDataObjChksumsTimeStampsFromAVU(), chrPtr1=(%s), ptrInpColl->collName=(%s)\n", chrPtr1, ptrInpColl->collName);  
+   if (chrPtr1 != NULL && *chrPtr1 == '/' && ((chrPtr1 - ptrInpColl->collName) <= (strlen(ptrInpColl->collName - 1)))) 
+     *chrPtr1 = 0; // replace '/' in /myzone/foo/'
+   printf ("GJK-P P.1.0.2. in iGetDataObjChksumsTimeStampsFromAVU(), chrPtr1=(%s), ptrInpColl->collName=(%s)\n", chrPtr1, ptrInpColl->collName);  
+
+   if ((iI = isData(rei->rsComm, ptrInpColl->collName, NULL)) >= 0) {
+     rodsLog (LOG_NOTICE, "GJK iGetDataObjChksumsTimeStampsFromAVU: input (%s) is data.", ptrInpColl->collName); 
+   }
+   else 
+     {
+       printf ("GJK-P P.21.0.2. in iGetDataObjChksumsTimeStampsFromAVU(), chrPtr1=(%s), ptrInpColl->collName=(%s), iI=%d\n", chrPtr1, ptrInpColl->collName, iI);  
+       if ((iI = isColl(rei->rsComm, ptrInpColl->collName, NULL)) < 0) {
+	 rodsLog (LOG_ERROR, 
+		  "iGetDataObjChksumsTimeStampsFromAVU: input object=(%s) is not data or collection. Exiting!", ptrInpColl->collName);
+	 //return (rei->status);
+       }
+       else {
+	 rodsLog (LOG_ERROR, 
+		  "GJK iGetDataObjChksumsTimeStampsFromAVU: input (%s) is a collection.", ptrInpColl->collName);
+	 return (rei->status);
+       }
+     }
+
+   // printf ("GJK-P P.1.0.3. iGetDataObjChksumsTimeStampsFromAVU : input (%s)", ptrInpColl->collName);
+   
+   if (rei->rsComm == NULL) {
+     rodsLog (LOG_ERROR,
+	      "GJKgetDataObjPSmeta: input rsComm is NULL");
+     return (SYS_INTERNAL_NULL_INPUT_ERR);
+   }
+   
+   memset (&genQueryInp, 0, sizeof (genQueryInp_t));
+
+   i1a[0]=COL_META_DATA_ATTR_NAME;
+   i1b[0]=0; /* currently unused */
+   i1a[1]=COL_META_DATA_ATTR_VALUE;
+   i1b[1]=0; /* currently unused */
+   i1a[2]=COL_META_DATA_ATTR_UNITS;
+   i1b[2]=0; /* currently unused */
+   genQueryInp.selectInp.inx = i1a;
+   genQueryInp.selectInp.value = i1b;
+   genQueryInp.selectInp.len = 3;
+
+   strncpy(strAbsPath, ptrInpColl->collName, MAX_NAME_LEN);
+   printf ("GJK-P P.14.0.11. in iGetDataObjChksumsTimeStampsFromAVU(), strAbsPath=(%s), ptrInpColl->collName=(%s)\n", 
+	   strAbsPath, ptrInpColl->collName);  
+
+   iErr = splitPathByKey(strAbsPath, strDirName, strFileName, '/');
+
+   printf ("GJK-P P.14.0.12. in iGetDataObjChksumsTimeStampsFromAVU(), strAbsPath=(%s), ptrInpColl->collName=(%s), strDirName=(%s), strFileName=(%s), iErr=%d\n", 
+	   strAbsPath, ptrInpColl->collName, strDirName, strFileName, iErr);  
+
+   i2a[0]=COL_COLL_NAME;
+   sprintf(v1,"='%s'",strDirName);
+   condVal[0]=v1;
+
+   i2a[1]=COL_DATA_NAME;
+   sprintf(v2,"='%s'",strFileName);
+   condVal[1]=v2;
+
+   genQueryInp.sqlCondInp.inx = i2a;
+   genQueryInp.sqlCondInp.value = condVal;
+   genQueryInp.sqlCondInp.len=2;
+
+   if (attrName != NULL && *attrName!='\0') {
+     i2a[2]=COL_META_DATA_ATTR_NAME;
+     sprintf(v3,"= '%s'",attrName);
+     condVal[2]=v3;
+     genQueryInp.sqlCondInp.len++;
+   }
+   
+   genQueryInp.maxRows=100;
+   genQueryInp.continueInx=0;
+   genQueryInp.condInput.len=0;
+
+   printf ("GJK-P P.14.0.13. in iGetDataObjChksumsTimeStampsFromAVU(), strAbsPath=(%s), ptrInpColl->collName=(%s), v3=(%s), iErr=%d\n", strAbsPath, ptrInpColl->collName, v3, iErr);  
+
+   /* Actual query happens here */
+   iErr = rsGenQuery(rei->rsComm, &genQueryInp, &genQueryOut);
+
+   printf ("GJK-P P.14.0.14. in iGetDataObjChksumsTimeStampsFromAVU(), strAbsPath=(%s), ptrInpColl->collName=(%s), iErr=%d\n", strAbsPath, ptrInpColl->collName, iErr);  
+
+   if (iErr == CAT_NO_ROWS_FOUND) {
+      i1a[0]=COL_D_DATA_PATH;
+      genQueryInp.selectInp.len = 1;
+      iErr = rsGenQuery(rei->rsComm, &genQueryInp, &genQueryOut);
+      if (iErr == 0) {
+	 printf("GJK GJKgetDataObjPSmeta(),  iErr=%d, None\n", iErr); 
+	 return(0);
+      }
+      if (iErr == CAT_NO_ROWS_FOUND) {
+
+	rodsLog (LOG_NOTICE, "GJKgetDataObjPSmeta: DataObject %s not found. iErr = %d", strAbsPath, iErr);
+	countUserDefinedMetadata = 0;
+	return (0);
+      }
+       printCount+=extractPSQueryResults(rei->rsComm, iErr, genQueryOut, byteBuf, strAbsPath); // proc??
+   }
+   else {
+     // printCount+=extractPSQueryResults(rei->rsComm, iErr, genQueryOut, byteBuf, strAbsPath); // proc??
+   }
+
+   while (iErr==0 && genQueryOut->continueInx > 0) {
+     genQueryInp.continueInx=genQueryOut->continueInx;
+     iErr = rsGenQuery(rei->rsComm, &genQueryInp, &genQueryOut);
+     printCount+= extractPSQueryResults(rei->rsComm, iErr, genQueryOut, byteBuf, strAbsPath); // proc ??
+   }
+   
+   return (iErr);
+}
+
+#define GJK44
+#ifdef GJK44
+/*
+ * Gets pipe separated metadata AVUs fgoor a data object.
+ * 
+ */
+int
+m2siGetDataObjChksumsTimeStampsFromAVU(msParam_t *inpParam, msParam_t *outParam, ruleExecInfo_t *rei)
 {
    genQueryInp_t genQueryInp;
    genQueryOut_t *genQueryOut;
@@ -56,7 +218,7 @@ int msiGetDataObjChksumsTimeStampsFromAVU(msParam_t* queryParam, msParam_t* genQ
   char units[NAME_MAX+1];
 };
 
- static struct UserDefinedMetadata UAVArray[1024];
+ static struct UserDefinedMetadata aAVUarray[1024];
  static int countUserDefinedMetadata;
 
    /* For testing mode when used with irule --test */
@@ -64,19 +226,17 @@ int msiGetDataObjChksumsTimeStampsFromAVU(msParam_t* queryParam, msParam_t* genQ
    
    printf ("GJK-P P.1.0.0. in msiGetDataObjChksumsTimeStampsFromAVU(), GJK msiGetDataObjChksumsTimeStampsFromAVU: GJK Calling msiGetDataObjChksumsTimeStampsFromAVU\n");  
    
-   return 0;
-
    // does NOT work !!! rodsLogAndErrorMsg (LOG_ERROR, &rsComm->rError, rei->status, "GJK msiGetDataObjChksumsTimeStampsFromAVU: GJK Calling msiGetDataObjChksumsTimeStampsFromAVU");
    
    rsComm = rei->rsComm;
    
-   /* parse queryParam */
-   rei->status = parseMspForCollInp (queryParam, &collInpCache, &outCollInp, 0);
+   /* parse inpParam1 */
+   rei->status = parseMspForCollInp (inpParam, &collInpCache, &outCollInp, 0);
    
 
    if (rei->status < 0) {
      rodsLog (LOG_ERROR, &rsComm->rError, rei->status,
-	      "GJK msiGetDataObjChksumsTimeStampsFromAVU: input queryParam error. status = %d", rei->status);
+	      "GJK msiGetDataObjChksumsTimeStampsFromAVU: input inpParam1 error. status = %d", rei->status);
      return (rei->status);
    }
    
@@ -212,12 +372,12 @@ int msiGetDataObjChksumsTimeStampsFromAVU(msParam_t* queryParam, msParam_t* genQ
    for (iGjk22 = 0; iGjk22 < countUserDefinedMetadata; iGjk22++) {
      
      snprintf (objPath, MAX_NAME_LEN,"|%s|%s|%s|\n",
-	       UAVArray[iGjk22].attribute, UAVArray[iGjk22].value, UAVArray[iGjk22].units);
+	       aAVUarray[iGjk22].attribute, aAVUarray[iGjk22].value, aAVUarray[iGjk22].units);
      strncat(strOut, objPath,MAX_NAME_LEN);
    }
    
    //   sprintf(strOut, "#1\n#2\n\n#3 lines gjk\n");
-   i = fillStrInMsParam (genQueryOutParam, strOut);  // MsParam.c parse  addformatedtrsing to bytes WriteBytesBuff printMsParam.c
+   i = fillStrInMsParam (outParam, strOut);  // MsParam.c parse  addformatedtrsing to bytes WriteBytesBuff printMsParam.c
    // fillBuffInParam
    
    //return(i);
@@ -225,4 +385,5 @@ int msiGetDataObjChksumsTimeStampsFromAVU(msParam_t* queryParam, msParam_t* genQ
    return (status);
 }
 
+#endif
 
