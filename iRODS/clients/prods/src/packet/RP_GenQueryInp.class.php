5bcce91bcc768ece09292dfbf6b3ffeedeaf41c0
sifang <sifang@sdsc.edu>
2007-10-05 22:13:37 +0000

    Bug fix
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@172 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/prods/src/packet/RP_GenQueryInp.class.php
+++ iRODS/clients/prods/src/packet/RP_GenQueryInp.class.php
@@ -9,9 +9,9 @@ class RP_GenQueryInp extends RODSPacket
     RP_InxValPair $InxValPair_PI=NULL,
     $options=0, $partialStartIndex=0)
   {
-    if (!isset($KeyValPair_PI)) $KeyValPair_PI=new KeyValPair_PI();
-    if (!isset($InxIvalPair_PI)) $InxIvalPair_PI=new InxIvalPair_PI();
-    if (!isset($InxValPair_PI)) $InxValPair_PI=new InxValPair_PI();
+    if (!isset($KeyValPair_PI)) $KeyValPair_PI=new RP_KeyValPair();
+    if (!isset($InxIvalPair_PI)) $InxIvalPair_PI=new RP_InxIvalPair();
+    if (!isset($InxValPair_PI)) $InxValPair_PI=new RP_InxValPair();
     
     $packlets=array("maxRows" => $maxRows, 'continueInx' => $continueInx,
       'partialStartIndex' => $partialStartIndex, 'options' => $options,
