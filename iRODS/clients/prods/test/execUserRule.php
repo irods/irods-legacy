9427389153ad211cd40d0a04c9219344be443d98
sifang <sifang@sdsc.edu>
2007-10-11 01:15:00 +0000

    Added code to test ProdsRule class
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@196 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/prods/test/execUserRule.php
+++ iRODS/clients/prods/test/execUserRule.php
@@ -7,11 +7,13 @@ $account=new RODSAccount("localhost", 1247, "rods", "RODS", "tempZone");
 $target_dir='/tempZone/home/rods/repl_test';
 $rule_body='myTestRule||'.
   'delayExec(<PLUSET>1m</PLUSET>,'.
-    'msiReplColl(*desc_coll,*desc_resc,backupMode,*outbuf),'.
+    'msiReplColl(*desc_coll1,*desc_resc,backupMode,*outbuf)'.
+    '##msiReplColl(*desc_coll2,*desc_resc,backupMode,*outbuf),'.
   'nop)|nop';
-$input_params=array('*desc_coll'=>'/tempZone/home/rods/repl_test',
+$input_params=array('*desc_coll1'=>'/tempZone/home/rods/repl_test3',
+  '*desc_coll2'=>'/tempZone/home/rods/repl_test4',
   '*desc_resc'=>'demoResc2');
-$out_params=array('*desc_coll');  
+$out_params=array('*desc_coll1','*desc_coll2');  
 
 try {
 $start_time=microtime(true);
@@ -20,7 +22,11 @@ $conn=new RODSConn($account);
 $conn->connect();
 $results=$conn->execUserRule($rule_body,$input_params,$out_params);
 var_dump($results);
+$conn->disconnect();
 
+$rule=new ProdsRule($account,$rule_body,$input_params,$out_params);
+$results=$rule->execute(); 
+var_dump($results);
 
 $end_time=microtime(true);
 $exec_time=$end_time-$start_time;
