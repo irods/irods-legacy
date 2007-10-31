e9679cac17de445cef294295ee0127134ad56cc9
awu <awu@sdsc.edu>
2007-10-31 23:10:24 +0000

    List of resources now retrieved from the database.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@256 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/web/applets/edu/sdsc/grid/gui/applet/UploadItem.java
+++ iRODS/clients/web/applets/edu/sdsc/grid/gui/applet/UploadItem.java
@@ -9,22 +9,57 @@
 
 package edu.sdsc.grid.gui.applet;
 
+import java.util.List;
 import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.net.URISyntaxException;
 
 /**
  *
  * @author awu
  */
 public class UploadItem implements AppletConstant {
-    private String source, destination, resource, type; // type has value FILE or FOLDER
+    private String source, destination, selectedResource, type; // type has value FILE or FOLDER
     private String status = STATUS_QUEUED; // queued, uploaded, in progress, requires authentication, server unavailable, failed
     private boolean assigned = false;
-   
+    private List resourceList;
+    static AppletLogger logger = AppletLogger.getInstance();
     
-    public UploadItem(String source, String destination, String resource) {
+    public UploadItem(String source, String destination, String selectedResource) {
         this.source = source;
         this.destination = destination;
-        this.resource = resource;
+        this.selectedResource = selectedResource;
+
+        try {
+            URI uri = new URI(destination);
+            
+            resourceList = new DBUtil().getResourceList(uri.getHost(), uri.getPort());
+        } catch (URISyntaxException e) {
+        }
+        //resourceList.add(selectedResource);
+        
+        // find out if this is a file or folder
+        File f = new File(source);
+        if (f.exists())
+            if (f.isFile())
+                type = TYPE_FILE;
+            else
+                type = TYPE_FOLDER;
+        // else
+        // TODO: Handle case when file no longer exists
+            
+    }//UploadItem
+    
+
+    public UploadItem(String source, String destination, List resourceList) {
+        this.source = source;
+        this.destination = destination;
+        this.resourceList = resourceList;
+        if (resourceList != null && resourceList.size() > 0)
+            this.selectedResource = (String) resourceList.get(0);
+        else
+            this.selectedResource = ""; // should never come here
         
         // find out if this is a file or folder
         File f = new File(source);
@@ -38,6 +73,7 @@ public class UploadItem implements AppletConstant {
             
     }//UploadItem
 
+
     public String getSource() {
         return source;
     }
@@ -45,9 +81,20 @@ public class UploadItem implements AppletConstant {
     public String getDestination() {
         return destination;
     }
+    
+    public String getDestinationParent() {
+        // returns the parent folder of the upload item
+        int lastIndex = destination.lastIndexOf("/");
+        return destination.substring(0, lastIndex);
+        
+    }
 
-    public String getResource() {
-        return resource;
+    public String getSelectedResource() {
+        return selectedResource;
+    }
+
+    public List getResourceList() {
+        return resourceList;
     }
 
     public String getType() {
