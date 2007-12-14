b49ece7ff48e9d4a33a65b794b33d817a126f993
nadeau <nadeau@sdsc.edu>
2007-12-14 20:54:05 +0000

    A rewrite of 'start.pl' to remove dependencies upon the user's PATH to find "ps" and other external commands.  Also, because use of #!/usr/bin/perl on the first line is not portable (Perl isn't always in /usr/bin), that feature has been removed.  As a result, this Perl script cannot be run directly.  Instead, run it via the front-end shell script 'start' or by the main 'irodsctl' script.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@291 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/bin/start.pl
+++ iRODS/server/bin/start.pl
@@ -1,78 +1,105 @@
-#!/usr/bin/perl
+#
+# Perl
 
-# Copyright (c), The Regents of the University of California            ***
-# For more information please refer to files in the COPYRIGHT directory ***
+# Start the iRODS server.
+#
+# Usage is:
+#	perl start.pl
+#
 
-# Start the irodsServer (which will start the agents).
-# The command line argument, if provided, is the irods server bin directory,
-# and if not provided it is assumed to be the current directory.
-# If irodsServer is not in that directory, then the location of this
-# script is used.
+use File::Spec;
+use Cwd;
+use Cwd "abs_path";
 
 
-($arg1)=@ARGV;
-if ($arg1) {
-    $serverBinDir = $arg1;
-    chdir "$arg1" || die "Can't chdir to input dir: $arg1";
+
+
+
+# Server names
+%servers = (
+	# Nice name		process name
+	"iRODS agents" =>	"irodsAgent",
+	"iRODS rule servers" =>	"irodsReServer",
+	"iRODS servers" =>	"irodsServer"
+);
+
+
+
+
+
+# Find the iRODS home directory and load the support script.
+#	This script may have been executed from the iRODS home, or
+#	from one of its subdirectories.
+$IRODS_HOME = cwd( );
+my $configFile = File::Spec->catfile( $IRODS_HOME, "config", "utils_platform.pl" );
+while ( ! -e $configFile )
+{
+	my $newHome = abs_path( File::Spec->catdir( $IRODS_HOME, ".." ) );
+	if ( $newHome eq $IRODS_HOME )
+	{
+		print( "Usage problem:\n" );
+		print( "    Please run this script from the iRODS home directory.\n" );
+		exit( 1 );
+	}
+	$IRODS_HOME = $newHome;
+	$configFile = File::Spec->catfile( $IRODS_HOME, "config", "utils_platform.pl" );
 }
-else {
-    $serverBinDir = `pwd`;
-    chomp($serverBinDir);
+
+require $configFile;
+
+
+
+
+
+# Make sure the server is available
+my $serverBinDir = File::Spec->catdir( $IRODS_HOME, "server", "bin" );
+my $irodsServer = File::Spec->catfile( $serverBinDir, "irodsServer" );
+if ( ! -e $irodsServer )
+{
+	print( "Usage problem:\n" );
+	print( "    The 'irodsServer' application could not be found.  Have the\n" );
+	print( "    iRODS servers been compiled?\n" );
+	exit( 1 );
 }
 
-# If the irodsServer is not in the provided serverBinDir then try
-# the directory that this script resides in.
-if (!-e "$serverBinDir/irodsServer") {
-    $oldSvr="$serverBinDir/irodsServer";
-    $startDir=`pwd`;
-    chomp($startDir);
-    if (index($0, "/") eq 0) {
-	$tmp= $0;
-    }
-    else {
-	$tmp=$startDir . "/" . $0;
-	chomp($tmp);
-    }
-    $i = rindex($tmp, "/");
-    $serverBinDir = substr($tmp, 0, $i);
-    if (!-e "$serverBinDir/irodsServer") {
-	print "Cannot find irodsServer\n";
-	print $oldSvr . " does not exist and\n";
-	print "$serverBinDir/irodsServer" . " does not exist\n";
-	exit -1;
-    }
-    chdir "$serverBinDir" || die "Can't chdir to: $serverBinDir";
-}
 
-# This is the umask for the server operation.
-umask 077;
 
-# irodsConfigDir is the irods server configuration directory where 
-# the server configuration files are located. The default
-# is "../config". 
-$irodsConfigDir=$serverBinDir . "/../config";
-$irodsLogDir=$serverBinDir . "/../log";
 
-# irodsEnvFile - The path for the .irodsEnv file which contains the Env info
-# of the user starting the irodsServer. The default is ~/.irods/.irodsEnv.
-# $irodsEnvFile=/tmp/.irodsEnv;
+# Prepare
+chdir( $serverBinDir );
+umask( 077 );
+
+# Directory containing server configuration 'server.config'.
+$irodsConfigDir = File::Spec->catdir( $IRODS_HOME, "server", "config" );
+
+# Directory for the server log.
+$irodsLogDir    = File::Spec->catdir( $IRODS_HOME, "server", "log" );
+
+# Optional iRODS environment override.  By default, the user's file
+# in '~/.irods/.irodsEnv' is used.
+# $irodsEnvFile = "/tmp/.irodsEnv";
 
 # irodsPort defines the port number the irodsServer is listening on.
 # The default port number is set in the .irodEnv file of the irods
 # admin starting the server. The irodsPort value set here (if set)
 # overrides the value set in the .irodEnv file.
-#
-# $irodsPort=5678;
+# $irodsPort = "5678";
 
 # spLogLevel defines the verbosity level of the server log. The levels are:
-# 9-LOG_SQL, 8-LOG_SYS_FATAL, 7-LOG_SYS_WARNING, 6-LOG_ERROR, 5-LOG_NOTICE,
-# 4- LOG_DEBUG, 3-LOG_DEBUG3, 2-LOG_DEBUG2, 1-LOG_DEBUG1. The lower the
-# level, the more verbose is the log. The default level is 5-LOG_NOTICE
-# $spLogLevel=3;
+#	9-LOG_SQL
+#	8-LOG_SYS_FATAL
+#	7-LOG_SYS_WARNING
+#	6-LOG_ERROR
+#	5-LOG_NOTICE
+#	4-LOG_DEBUG
+#	3-LOG_DEBUG3
+#	2-LOG_DEBUG2
+#	1-LOG_DEBUG1.
+# The lower the level, the more verbose is the log. The default level is 5-LOG_NOTICE
+# $spLogLevel = "3";
 
-# spLogSql defines if sql will be logged or not.
-# the default value is no logging.
-# $spLogSql=1;
+# spLogSql defines if sql will be logged or not.  The default is no logging.
+# $spLogSql = "1";
 
 # svrPortRangeStart and svrPortRangeEnd - A range of port numbers can be 
 # specified for the server's parallel I/O communication port. 
@@ -85,7 +112,7 @@ $irodsLogDir=$serverBinDir . "/../log";
 # to be run on the same host as the IES (ICAT enabled server). Please note that
 # only one irodsReServer should be run in each zone. reServerOnIes is on
 # by default
-$reServerOnIes=1;
+$reServerOnIes = 1;
 
 # reServerOnThisServer - Specifies that the delayed rule exec server 
 # (irodsReServer) to be run on this server. This allows the irodsReServer
@@ -107,41 +134,39 @@ $svrPortReconnect=1;
 # use 1 to make it log
 # $RETESTFLAG=1;
 
-$ENV{'irodsConfigDir'}=$irodsConfigDir;
-$ENV{'irodsLogDir'}=$irodsLogDir;
-if ($irodsEnvFile)      { $ENV{'irodsEnvFile'}=$irodsEnvFile; }
-if ($irodsPort)         { $ENV{'irodsPort'}=$irodsPort; }
-if ($spLogLevel)        { $ENV{'spLogLevel'}=$spLogLevel; }
-if ($spLogSql)        { $ENV{'spLogSql'}=$spLogSql; }
-if ($svrPortRangeStart) { $ENV{'svrPortRangeStart'}=$svrPortRangeStart; }
-if ($svrPortRangeEnd)   { $ENV{'svrPortRangeEnd'}=$svrPortRangeEnd; }
-if ($reServerOnIes)   { $ENV{'reServerOnIes'}=$reServerOnIes; }
-if ($reServerOnThisServer)   { $ENV{'reServerOnThisServer'}=$reServerOnThisServer; }
-if ($reServerOption)   { $ENV{'reServerOption'}=$reServerOption; }
-if ($svrPortReconnect)  {$ENV{'svrPortReconnect'}=$svrPortReconnect; }
-if ($RETESTFLAG)  {$ENV{'RETESTFLAG'}=$RETESTFLAG; }
-
-print "Starting: $serverBinDir/irodsServer\n";
-`$serverBinDir/irodsServer`;
-$status=$?;
-if ($status) {
-    print "irodsServer failed to start\n";
-    exit -1;
-}
-print "Sleeping 2 seconds\n";
-sleep 2;
-$curLogFile=`ls -1tr $irodsLogDir|grep rodsLog|tail -1`;
-#print $curLogFile;
-$tailLog=`tail $irodsLogDir/$curLogFile`;
-print "tail of current log file:\n" . $tailLog;
-$hostOS=`uname -s`;
-chomp($hostOS);
-if ("$hostOS" eq "Darwin") {
-    $ps=`ps -ax | grep -v grep | grep irodsServer`;
-}
-else {
-    $ps=`ps -ef | grep -v grep | grep irodsServer`;
-}
-print "\nps of running servers:\n" . $ps;
+				  $ENV{'irodsConfigDir'}      = $irodsConfigDir;
+if ($irodsEnvFile)		{ $ENV{'irodsEnvFile'}        = $irodsEnvFile; }
+if ($irodsPort)			{ $ENV{'irodsPort'}           = $irodsPort; }
+if ($spLogLevel)		{ $ENV{'spLogLevel'}          = $spLogLevel; }
+if ($spLogSql)			{ $ENV{'spLogSql'}            = $spLogSql; }
+if ($svrPortRangeStart)		{ $ENV{'svrPortRangeStart'}   = $svrPortRangeStart; }
+if ($svrPortRangeEnd)		{ $ENV{'svrPortRangeEnd'}     = $svrPortRangeEnd; }
+if ($reServerOnIes)		{ $ENV{'reServerOnIes'}       = $reServerOnIes; }
+if ($reServerOnThisServer)	{ $ENV{'reServerOnThisServer'}= $reServerOnThisServer; }
+if ($reServerOption)		{ $ENV{'reServerOption'}      = $reServerOption; }
+if ($svrPortReconnect)		{ $ENV{'svrPortReconnect'}    = $svrPortReconnect; }
+if ($RETESTFLAG)		{ $ENV{'RETESTFLAG'}          = $RETESTFLAG; }
 
 
+
+
+# Start the server
+system( $irodsServer );
+if ( $? )
+{
+	print( "iRODS server failed to start\n" );
+	exit -1;
+}
+
+@pids = getProcessIds( "irodsServer" );
+if ( $#pids < 0 )
+{
+	print( "iRODS server failed to start.\n" );
+	exit( -1 );
+}
+foreach $pid (@pids)
+{
+	print( "Process $pid.  iRODS server is running.\n" );
+}
+
+exit( 0 );
