e2d87cb2c7c746a7e5fac155524423c035a719a5
sifang <sifang@sdsc.edu>
2008-03-25 02:37:23 +0000

    Added option 'AUTO_EXTRACT_EXIF', for whether EXIF tag should be extracted and set as metadata at upload time. Default is false.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@567 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/web/config.inc.php
+++ iRODS/clients/web/config.inc.php
@@ -1,6 +1,11 @@
 <?php
   define("PRODS_INC_PATH", dirname(__FILE__)."/../prods/src/Prods.inc.php");
   
+  // automatically extract exif headers from tiff and jpeg.
+  // note that this option requirs PHP EXIF package to be configured at install time:
+  // http://us2.php.net/exif
+  define("AUTO_EXTRACT_EXIF", false);
+  
   /*
   define("DEFAULT_RODS_HOST","rt.sdsc.edu");
   define("DEFAULT_RODS_PORT",1247);
