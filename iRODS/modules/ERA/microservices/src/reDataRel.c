2903012e5db8643eb180882bbe2e3a86cebb04dc
kremenek <kremenek@sdsc.edu>
2008-03-28 21:21:05 +0000

    Adding a new module
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@589 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/modules/ERA/microservices/src/reDataRel.c
+++ iRODS/modules/ERA/microservices/src/reDataRel.c
@@ -1,7 +1,7 @@
 /*** Copyright (c), The Regents of the University of California            ***
  *** For more information please refer to files in the COPYRIGHT directory ***/
 
-/* working, DRAFT, versio, It WILL be cleaned! */
+/* in modules/persistentArchives/microservices/src/reDataRel.c, working, DRAFT, versio, It WILL be cleaned! */
 
 #include <stdarg.h>
 
