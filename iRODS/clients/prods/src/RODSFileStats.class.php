9ce3fe3b2668335a4a885e9654d87d402504e538
sifang <sifang@sdsc.edu>
2008-04-12 01:26:23 +0000

    Added new field "ownerzone"
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@614 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/prods/src/RODSFileStats.class.php
+++ iRODS/clients/prods/src/RODSFileStats.class.php
@@ -5,18 +5,20 @@ class RODSFileStats
   public $name;
   public $size;
   public $owner;
+  public $ownerzone;
   public $mtime;
   public $ctime;
   public $id;
   public $typename;  
   public $rescname;
   
-  public function __construct($name,$size,$owner,$mtime,$ctime,$id,$typename,
+  public function __construct($name,$size,$owner,$ownerzone, $mtime,$ctime,$id,$typename,
     $rescname)
   {
     $this->name=$name;
     $this->size=$size;
     $this->owner=$owner;
+    $this->ownerzone=$ownerzone;
     $this->mtime=$mtime;
     $this->ctime=$ctime;
     $this->id=$id;
