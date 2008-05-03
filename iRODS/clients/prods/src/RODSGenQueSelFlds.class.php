11fc1a5fb63e11a2d5c40a7e229f0992622a049a
sifang <sifang@sdsc.edu>
2008-05-03 01:05:01 +0000

    Added supported attributes: min, max, avg, count, sum Added method update() to update an attribute, with OR Added mothod attr2GenQueNumber() to convert attribute (string) to iRODS general query numbers
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@679 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/prods/src/RODSGenQueSelFlds.class.php
+++ iRODS/clients/prods/src/RODSGenQueSelFlds.class.php
@@ -10,8 +10,13 @@ class RODSGenQueSelFlds
   * default constructor.
   * @param string $name name of the field, which must be one defined in file 'RodsGenQueryNum.inc.php'.
   * @param string $attrs extra options used for operations such as order_by, the expected values are:
-  *     - 'order_by_asc' order the result by this field, in ASCENDING order
-  *     - 'order_by_desc' order the result by this field, in DESCENDING order
+  * - 'order_by_asc' order the result by this field, in ASCENDING order
+  * - 'order_by_desc' order the result by this field, in DESCENDING order
+  * - min minimum of the group
+  * - max maximum of the group
+  * - sum sum of the group
+  * - avg average of the group
+  * - count count of the group
   */
   public function __construct(array $names=array(), array $attrs=array())    
   {
@@ -30,31 +35,15 @@ class RODSGenQueSelFlds
           'PERR_USER_INPUT_ERROR');
       }
       $this->indexes[]=$GLOBALS['PRODS_GENQUE_NUMS']["$name"];
-      
-      if (empty($attrs[$i]))
-        $this->attrs[]=1;
-      else
-      if ($attrs[$i]=='order_by_asc')
-      {
-        $this->attrs[]=$GLOBALS['PRODS_GENQUE_NUMS']['ORDER_BY'];
-      }
-      else
-      if ($attrs[$i]=='order_by_desc')
-      {
-        $this->attrs[]=$GLOBALS['PRODS_GENQUE_NUMS']['ORDER_BY_DESC'];
-      }
-      else
-      {
-        $attr=$attrs[$i];
-        throw new RODSException("Unexpected attribute: '$attr'",
-            'PERR_USER_INPUT_ERROR');
-      }  
+      $this->attrs[]=RODSGenQueSelFlds::attr2GenQueNumber($attrs[$i]);
     }
     
   }
   
  /**
   * Add a single select field.
+  *
+  * @param string name name of the field, which must be one defined in file 'RodsGenQueryNum.inc.php'.
   */
   public function add($name, $attr=NULL)
   {
@@ -66,28 +55,85 @@ class RODSGenQueSelFlds
     }
     $this->indexes[]=$GLOBALS['PRODS_GENQUE_NUMS']["$name"];
     $this->names[]=$name;
-    
-    if (empty($attrs))
-      $this->attrs[]=1;
-    else
-    if ($attr=='order_by_asc')
-    {
-      $this->attrs[]=$GLOBALS['PRODS_GENQUE_NUMS']['ORDER_BY'];
-    }
-    else
-    if ($attr=='order_by_desc')
-    {
-      $this->attrs[]=$GLOBALS['PRODS_GENQUE_NUMS']['ORDER_BY_DESC'];
-    }
-    else
-    {
-      throw new RODSException("Unexpected attribute: '$attr'",
-          'PERR_USER_INPUT_ERROR');
-    }  
+    $this->attrs[]=RODSGenQueSelFlds::attr2GenQueNumber($attr);
   }
   
  /**
-  * make a RP_InxIvalPair.
+  * update a single select field's attr/value. Note that if the value already exists, 
+  * it will OR the bits. This is used when you want more than one type of operation
+  * for a select field, such as select_max and sort.
+  */
+  public function update($name, $attr)
+  {
+    require_once("RodsGenQueryNum.inc.php"); //load magic numbers 
+    if(!isset($GLOBALS['PRODS_GENQUE_NUMS']["$name"]))
+    {
+      throw new RODSException("General Query select field name '$name' is not valid",
+          'PERR_USER_INPUT_ERROR');
+    }
+    
+    $newattr=RODSGenQueSelFlds::attr2GenQueNumber($attr);
+    for($i=0; $i<count($this->names); $i++)
+    {
+      if ($this->names[$i]==$name)
+      {
+        if ($this->attrs[$i]==1)
+          $this->attrs[$i]=$newattr;
+        else
+          $this->attrs[$i]=$newattr | $this->attrs[$i];
+        return;
+      }
+    }
+    $this->add($name, $attr);
+  }
+  
+ /**
+  * Convert supported attribute to magic number, that iRODS protocol uses
+  * Following attributes are supported: 
+  * - 'order_by_asc' order the result by this field, in ASCENDING order
+  * - 'order_by_desc' order the result by this field, in DESCENDING order
+  * - min minimum of the group
+  * - max maximum of the group
+  * - sum sum of the group
+  * - avg average of the group
+  * - count count of the group
+  */
+  public static function attr2GenQueNumber($attr)
+  {
+    if (empty($attr)) return 1;
+    $retval=1;
+    switch($attr)
+    {
+      case 'order_by_asc':
+        $retval=$GLOBALS['PRODS_GENQUE_NUMS']['ORDER_BY'];
+        break;
+      case 'order_by_desc':
+        $retval=$GLOBALS['PRODS_GENQUE_NUMS']['ORDER_BY_DESC'];
+        break;
+      case 'min':
+        $retval=$GLOBALS['PRODS_GENQUE_NUMS']['SELECT_MIN'];
+        break;  
+      case 'max':
+        $retval=$GLOBALS['PRODS_GENQUE_NUMS']['SELECT_MAX'];
+        break;  
+      case 'sum':
+        $retval=$GLOBALS['PRODS_GENQUE_NUMS']['SELECT_SUM'];
+        break;    
+      case 'avg':
+        $retval=$GLOBALS['PRODS_GENQUE_NUMS']['SELECT_AVG'];
+        break;   
+      case 'count':
+        $retval=$GLOBALS['PRODS_GENQUE_NUMS']['SELECT_COUNT'];
+        break;  
+      default:
+        throw new RODSException("Unexpected attribute: '$attr'",
+          'PERR_USER_INPUT_ERROR');
+    }
+    return intval($retval);    
+  }
+  
+ /**
+  * make a RP_InxIvalPair, a low level iRODS packet
   */
   public function packetize()
   {
