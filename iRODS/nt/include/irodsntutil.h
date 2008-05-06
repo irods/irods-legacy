acfdef87ed32b8f52eb6c09332e868763909dd11
bzhu <zhu@sciences.sdsu.edu>
2008-05-06 23:59:38 +0000

    windows util functions
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@690 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/nt/include/irodsntutil.h
+++ iRODS/nt/include/irodsntutil.h
@@ -2,37 +2,37 @@
 #define _IRODSNTUTIL_H_
 
 #include <string.h>
+/*
 #include <io.h>
 #include <fcntl.h>
 #include <windows.h>
+*/
 #include "stdlib.h"
 #include "stdio.h"
+/*
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <conio.h>
+#include <direct.h>
+*/
 
 #ifdef  __cplusplus
 extern "C" {
 #endif
 
-int iRODSNtFileOpen(const char *filename,int oflag, int istextfile);
-void iRODSPathToNtPath(char *ntpath,const char *srbpath);;
-static void StrChangeChar(char* str,char from, char to);
-void iRODSNTPathBackSlash(char *str);
-void iRODSNTPathForwardSlash(char *str);
-FILE *iRODSNT_fopen(const char *filename, const char *mode);
 void iRODSPathToNtPath(char *ntpath,const char *srbpath);
 int iRODSNtFileOpen(const char *filename,int oflag, int istextfile);
-int iRODSNTFileBinaryOpen(const char *filename,int oflag);
-int iRODSNTFileBinaryCreate(const char *filename);
-int iRODSNTUnlinkFile(char *filename);
-void iRODSNTCheckExecMode(int aargc,char **aargv);
-int iRODSNTRunInConsoleMode();
-void NtEmergencyMessage(char *msg);
+void iRODSNTPathBackSlash(char *str);
+void iRODSNTPathForwardSlash(char *str);
+FILE *iRODSNt_fopen(const char *filename, const char *mode);
+int iRODSNt_pen(const char *filename,int oflag, int istextfile);
+int iRODSNt_bpen(const char *filename,int oflag);
+int iRODSNt_bcreate(const char *filename);
+int iRODSNt_unlink(char *filename);
+int iRODSNt_stat(const char *filename,struct stat *stat_p);
+
+
 void iRODSNTGetUserPasswdInputInConsole(char *buf, char *prompt);
-long getppid(void);
-int getopt(int argc, char* const argv[], const char* optstring);
-int NTmkdir(const char *dirname);
 #ifdef  __cplusplus
 }
 #endif
