e2e04050f66fcad58c2d709825e467630e4b2641
bzhu <zhu@sciences.sdsu.edu>
2008-05-16 21:06:56 +0000

    c style declaration for c++
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@704 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/nt/include/startsock.h
+++ iRODS/nt/include/startsock.h
@@ -3,6 +3,8 @@
 
 #ifdef _WIN32
 
+
+
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
@@ -14,11 +16,20 @@
 /* #define errno WSAGetLastError() */
 #define COMM_BUF_SIZE 512
 
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
 int startWinsock();
 int cleanWinsock();
 SOCKET duplicateWinsock(SOCKET oldSocket);
 
+#ifdef  __cplusplus
+}
 #endif
+
+#endif
+
 
 
 #endif
