d6fec8484eb1d00ec53f0830988399f4b72c3bb4
Wayne-Schroeder <schroeder@diceresearch.org>
2008-06-24 21:13:41 +0000

    Remove the 'quit' line at the end; causes an error and is unneeded.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@919 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/icat/src/psg-patch-v1.0Tov1.1.sql
+++ iRODS/server/icat/src/psg-patch-v1.0Tov1.1.sql
@@ -5,5 +5,3 @@ alter table R_RULE_EXEC add column rule_name varchar(2700);
 
 delete from R_TOKN_MAIN  where token_id = 1700;
 insert into R_TOKN_MAIN values ('action_type',1800,'generic','','','','','1170000000','1170000000');
-
-quit
