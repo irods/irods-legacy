4afb2ae3b304925b25d60f4603dc9510ee3e9488
lindsey <susanunit@gmail.com>
2008-07-09 18:20:31 +0000

    - Modifying & adding to concurrent test scripts utilities
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@952 ea137bd6-aa86-47b6-b90d-b101d9f19a90

---
+++ iRODS/clients/concurrent-test/src/writebigfile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main (int argc, char** argv) {
+
+	FILE* fp;
+	int	i;
+	char x[256];
+
+	fp = fopen ("bigfile", "w");
+
+	for (i=0; i<256; i++)
+		x[i]=(char) i;
+
+	for (i=0; i<1000000; i++)
+		fwrite(x, sizeof(x[0]), sizeof(x)/sizeof(x[0]), fp);
+
+	fclose (fp);
+
+}
