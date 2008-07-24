30b7a035077f5accf27c0893153085e6731db1d9
lindsey <susanunit@gmail.com>
2008-07-24 16:06:08 +0000

    - Access to data complete, now just need to format nicely
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1004 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/modules/integrityChecks/microservices/src/integrityChecksMS.c
+++ iRODS/modules/integrityChecks/microservices/src/integrityChecksMS.c
@@ -13,8 +13,13 @@ int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3,
 	char collname[200];
 	int maxfilesize, minfilesize;
 	bytesBuf_t	*mybuf;
-	// int i;
-	int j;
+	int i,j;
+	sqlResult_t *dataName;
+	sqlResult_t *dataSize;
+	sqlResult_t *collName;
+	
+
+	char* attrs[]={"filename", "filesize", "collname"};
 
 	RE_TEST_MACRO ("    Calling msiCheckCollectionFilesizeRange")
 
@@ -33,7 +38,7 @@ int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3,
 
 	rodsLog (LOG_ERROR, "-1 s tuff: ");
 
-	memset (&genQueryInp, 0, sizeof (genQueryInp_t));
+	memset (&genQueryInp, 0, sizeof(genQueryInp_t));
 	genQueryInp.maxRows = MAX_SQL_ROWS;
 
 	/* this is the info we want returned from the query */
@@ -41,48 +46,54 @@ int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3,
 	addInxIval (&genQueryInp.selectInp, COL_DATA_SIZE, 1);
 	addInxIval (&genQueryInp.selectInp, COL_COLL_NAME, 1);
 
-	/* make the condition */
-
 	rodsLog (LOG_ERROR, "0 stuff: ");
 
+	/* make the condition */
 	snprintf (condStr, MAX_NAME_LEN, " = '%s'", collectionname);
 	addInxVal (&genQueryInp.sqlCondInp, COL_COLL_NAME, condStr); /* what's this? */
 	rodsLog (LOG_ERROR, "1 s tuff: ");
 
-	//j = rsGenQuery (rsComm, &genQueryInp, genQueryOut);
+	fprintf (stderr,"\n");
+	printGenQI(&genQueryInp);  
+	fprintf (stderr,"\n");
+	rodsLog (LOG_ERROR, "2 s tuff: ");
+
+	j = rsGenQuery (rsComm, &genQueryInp, &genQueryOut);
 	rodsLog (LOG_ERROR, "3 s tuff: ");
 
-	j=0;
 	if (j != CAT_NO_ROWS_FOUND) {
 
-		bytesBuf_t	*buf_p;
-		buf_p = mybuf;
-
-	rodsLog (LOG_ERROR, "4 s tuff: ");
-		mybuf->buf = (void*) malloc (1000);
-		strncpy (mybuf->buf, "butter", strlen("butter"));
-
-	rodsLog (LOG_ERROR, "5 s tuff: ");
 		/* we got results - do something cool */
-/*
-		for (i=0; i<genQueryOut->totalRowCount; i++) {
-			memcpy (buf_p->buf, genQueryOut->sqlResult[i], sizeof (sqlResult_t));
-			buf_p += sizeof (sqlResult_t);
+		/* just print out the GenQueryOut struct sheesh */
+		fprintf (stderr, "GenQO->rowCnt: %d\n", genQueryOut->rowCnt);
+		fprintf (stderr, "GenQO->totalRowCount: %d\n", genQueryOut->totalRowCount);
+		fprintf (stderr, "GenQO->attriCnt: %d\n", genQueryOut->attriCnt);
+		for (i=0; i<genQueryOut->rowCnt; i++) {
+			fprintf (stderr, "genQO->sqlResult[%d].attriInx= %d\n", i,genQueryOut->sqlResult[i].attriInx);
+			fprintf (stderr, "genQO->sqlResult[%d].len= %d\n", i,genQueryOut->sqlResult[i].len);
+			fprintf (stderr, "genQO->sqlResult[%d].value= %s\n", i,genQueryOut->sqlResult[i].value);
 		}
-*/
+		dataName = getSqlResultByInx (genQueryOut, COL_DATA_NAME);
+		for (i=0; i<genQueryOut->rowCnt; i++) {
+			fprintf (stderr, "dataName[%d]:%s\n",i,&dataName->value[dataName->len * i]);
+		}
+		
+		dataSize = getSqlResultByInx (genQueryOut, COL_DATA_SIZE);
+		for (i=0; i<genQueryOut->rowCnt; i++) {
+			fprintf (stderr, "dataSize[%d]:%s\n",i,&dataSize->value[dataSize->len * i]);
+		}
+		collName = getSqlResultByInx (genQueryOut, COL_COLL_NAME);
+		for (i=0; i<genQueryOut->rowCnt; i++) {
+			fprintf (stderr, "collName[%d]:%s\n",i,&collName->value[collName->len * i]);
+		}
 	}
-
-/*
-	printGenQI(genQueryInp);  
-	printGenQI(genQueryOut);  
-*/
 
 	/* OK now copy over the results of the query....maybe */
 
 
 	rodsLog (LOG_ERROR, "6 s tuff: ");
 	/* fill in the return value struct */
-	fillStrInMsParam (mPout1, "some stuff");
+	fillStrInMsParam (mPout1, genQueryOut->sqlResult[0].value);
 	rodsLog (LOG_ERROR, "7 s tuff: ");
   
 //	return(rei->status);
