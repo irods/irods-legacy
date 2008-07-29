123bb960f1edc253db52a0b33d9ba07b507e905f
lindsey <susanunit@gmail.com>
2008-07-29 04:57:54 +0000

    - msiCheckFilesizeRange microservice and rule working.  Output not pretty.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1013 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/modules/integrityChecks/microservices/src/integrityChecksMS.c
+++ iRODS/modules/integrityChecks/microservices/src/integrityChecksMS.c
@@ -1,22 +1,27 @@
 #include "rsApiHandler.h"
 #include "integrityChecksMS.h"
 
-const char collectionname[]="/tempZone/home/rods";
-
 
 int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3, msParam_t *mPout1, ruleExecInfo_t *rei) {
 
 	genQueryInp_t genQueryInp;
-	genQueryOut_t *genQueryOut;
+	genQueryOut_t *genQueryOut = NULL;
 	char condStr[MAX_NAME_LEN];
 	rsComm_t *rsComm;
 	char collname[200];
-	int maxfilesize, minfilesize;
+	char maxfilesize[100]; 
+	char minfilesize[100];
 	bytesBuf_t	*mybuf;
 	int i,j;
 	sqlResult_t *dataName;
 	sqlResult_t *dataSize;
 	sqlResult_t *collName;
+
+	sqlResult_t	*v[MAX_SQL_ATTR];
+	char* colname[MAX_SQL_ATTR];
+	keyValPair_t	*results;	
+	char* key;
+	char* value;
 	
 
 	char* attrs[]={"filename", "filesize", "collname"};
@@ -33,10 +38,12 @@ int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3,
 
 	/* construct an SQL query from the parameter list */
 	strcpy (collname,  (char*) mPin1->inOutStruct);
-	maxfilesize  = (int) atoi(mPin2->inOutStruct);
-	minfilesize  = (int) atoi(mPin3->inOutStruct);
+	strcpy (maxfilesize, (char*) mPin2->inOutStruct);
+	strcpy (minfilesize, (char*) mPin3->inOutStruct);
 
-	rodsLog (LOG_ERROR, "-1 s tuff: ");
+	// initialize results to 0; AddKeyVal does all our malloc-ing
+	results = (keyValPair_t*) malloc (sizeof(keyValPair_t));
+	memset (results, 0, sizeof(keyValPair_t));
 
 	memset (&genQueryInp, 0, sizeof(genQueryInp_t));
 	genQueryInp.maxRows = MAX_SQL_ROWS;
@@ -49,8 +56,13 @@ int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3,
 	rodsLog (LOG_ERROR, "0 stuff: ");
 
 	/* make the condition */
-	snprintf (condStr, MAX_NAME_LEN, " = '%s'", collectionname);
-	addInxVal (&genQueryInp.sqlCondInp, COL_COLL_NAME, condStr); /* what's this? */
+	snprintf (condStr, MAX_NAME_LEN, " = '%s'", collname);
+	addInxVal (&genQueryInp.sqlCondInp, COL_COLL_NAME, condStr); 
+	snprintf (condStr, MAX_NAME_LEN, " < '%s'", minfilesize);
+	addInxVal (&genQueryInp.sqlCondInp, COL_DATA_SIZE, condStr); 
+	snprintf (condStr, MAX_NAME_LEN, " > '%s'", maxfilesize);
+	addInxVal (&genQueryInp.sqlCondInp, COL_DATA_SIZE, condStr); 
+	
 	rodsLog (LOG_ERROR, "1 s tuff: ");
 
 	fprintf (stderr,"\n");
@@ -59,7 +71,7 @@ int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3,
 	rodsLog (LOG_ERROR, "2 s tuff: ");
 
 	j = rsGenQuery (rsComm, &genQueryInp, &genQueryOut);
-	rodsLog (LOG_ERROR, "3 s tuff: ");
+	rodsLog (LOG_ERROR, "3 s tuff: j=%d",j);
 
 	if (j != CAT_NO_ROWS_FOUND) {
 
@@ -68,36 +80,72 @@ int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3,
 		fprintf (stderr, "GenQO->rowCnt: %d\n", genQueryOut->rowCnt);
 		fprintf (stderr, "GenQO->totalRowCount: %d\n", genQueryOut->totalRowCount);
 		fprintf (stderr, "GenQO->attriCnt: %d\n", genQueryOut->attriCnt);
+
+		rodsLog (LOG_ERROR, "4 stuff: ");
+
 		for (i=0; i<genQueryOut->rowCnt; i++) {
 			fprintf (stderr, "genQO->sqlResult[%d].attriInx= %d\n", i,genQueryOut->sqlResult[i].attriInx);
 			fprintf (stderr, "genQO->sqlResult[%d].len= %d\n", i,genQueryOut->sqlResult[i].len);
 			fprintf (stderr, "genQO->sqlResult[%d].value= %s\n", i,genQueryOut->sqlResult[i].value);
 		}
+
+		rodsLog (LOG_ERROR, "4.5 stuff: ");
+
 		dataName = getSqlResultByInx (genQueryOut, COL_DATA_NAME);
-		for (i=0; i<genQueryOut->rowCnt; i++) {
-			fprintf (stderr, "dataName[%d]:%s\n",i,&dataName->value[dataName->len * i]);
-		}
-		
 		dataSize = getSqlResultByInx (genQueryOut, COL_DATA_SIZE);
 		for (i=0; i<genQueryOut->rowCnt; i++) {
+			fprintf (stderr, "dataName[%d]:%s\n",i,&dataName->value[dataName->len * i]);
+			key = strdup (&dataName->value[dataName->len *i]);
 			fprintf (stderr, "dataSize[%d]:%s\n",i,&dataSize->value[dataSize->len * i]);
+			value = strdup (&dataSize->value[dataSize->len * i]);
+			addKeyVal (results, key, value);
 		}
+
+		rodsLog (LOG_ERROR, "4.8 stuff: ");
+
+/*
 		collName = getSqlResultByInx (genQueryOut, COL_COLL_NAME);
 		for (i=0; i<genQueryOut->rowCnt; i++) {
 			fprintf (stderr, "collName[%d]:%s\n",i,&collName->value[collName->len * i]);
 		}
+*/
+
 	}
 
-	/* OK now copy over the results of the query....maybe */
+	rodsLog (LOG_ERROR, "6 stuff: ");
+	printGenQueryOut(stderr, NULL, NULL, genQueryOut);
 
+	/* fill in the return value struct - but how?*/
+	// we'll use the KeyValPair_t struct - but is this optimal?
+	// OK traverse the GenQueryOut struct again - this time I'm going to do it all in my head!
+/*
+	for (i=0; i<genQueryOut->attriCnt; i++) {
+		v[i]=&genQueryOut->sqlResult[i];	
+		cname[i] = getAttrNameFromAttrId(v[i]->attriInx);
+	}
 
-	rodsLog (LOG_ERROR, "6 s tuff: ");
-	/* fill in the return value struct */
-	fillStrInMsParam (mPout1, genQueryOut->sqlResult[0].value);
+	for (i=0; i<genQueryOut->rowCnt; i++) {
+		for (j=0; j<genQueryOut->attriCnt; j++) {
+			fprintf (fd,"%s = %s\n", cname[j], &v[j]->value[v[j]->len * i]);
+			key = strdup ();
+		}
+	}
+*/
+
+	/* print out KeyVal struct */
+	rodsLog (LOG_ERROR, "6.5 stuff: ");
+	fprintf (stderr, "results.len: %d\n", results->len);
+	rodsLog (LOG_ERROR, "6.6 stuff: ");
+	for (i=0; i<results->len; i++) {
+		fprintf (stderr, "key: %s = value: %s\n", results->keyWord[i], results->value[i]);
+	}
+
+	fillMsParam (mPin3, NULL, KeyValPair_MS_T, results, NULL);
 	rodsLog (LOG_ERROR, "7 s tuff: ");
+	fillIntInMsParam (mPout1, rei->status);
+	rodsLog (LOG_ERROR, "8 s tuff: ");
   
-//	return(rei->status);
-	return (0);
+	return(rei->status);
 
 }
 
