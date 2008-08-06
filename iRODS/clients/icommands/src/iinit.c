cce26884487bdb29e96da320afa35520c59a946d
Wayne-Schroeder <schroeder@diceresearch.org>
2008-08-06 17:30:24 +0000

    If some key irods environment variables are missing, prompt for them and save them into ~/.irods/.irodsEnv (via appendRodEnv) if the connect/login works.  Also (first) create the ~/.irods directory if it is missing.  Not tested yet on Windows.
    We expect this will be most useful on Windows and Macs where creating
    the .irods/.irodsEnv file can tedious, especially for novice users.
    
    
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1040 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/icommands/src/iinit.c
+++ iRODS/clients/icommands/src/iinit.c
@@ -5,6 +5,47 @@
 
 void usage (char *prog);
 
+#define TTYBUF_LEN 100
+#define UPDATE_TEXT_LEN NAME_LEN*10
+
+/* 
+ Attempt to make the ~/.irods directory in case it doesn't exist (may
+ be needed to write the .irodsA file and perhaps the .irodsEnv file).
+ */
+int
+mkrodsdir() {
+   char dirName[NAME_LEN];
+   int status, mode;
+   char *getVar;
+#ifdef windows_platform
+   getVar = iRODSNt_gethome();
+#else
+   getVar = getenv("HOME");
+#endif
+   rstrcpy(dirName, getVar, NAME_LEN);
+   rstrcat(dirName, "/.irods", NAME_LEN);
+   mode = 0700;
+#ifdef _WIN32
+   status = iRODSNt_mkdir (dirName, mode);
+#else
+   status = mkdir (dirName, mode);
+#endif
+   return(0); /* no error messages as it normally fails */
+}
+
+void
+printUpdateMsg()
+{
+#if 0
+   printf("One or more fields in your iRODS environment file (.irodsEnv) are\n");
+   printf("missing.  This program will ask you for them and if they work (the\n");
+   printf("connection/login succeeds), it will update (or create if needed) your\n");
+   printf("irods environment file with those values for use by the i-commands.\n");
+   printf("You can also edit the environment file with a text editor.\n");
+#endif
+   printf("One or more fields in your iRODS environment file (.irodsEnv) are\n");
+   printf("missing; please enter them.\n");
+}
 int
 main(int argc, char **argv)
 {
@@ -17,6 +58,9 @@ main(int argc, char **argv)
     rodsArguments_t myRodsArgs;
     int useGsi=0;
     int doPassword;
+    char ttybuf[TTYBUF_LEN];
+    int doingEnvFileUpdate=0;
+    char updateText[UPDATE_TEXT_LEN]="";
 
     status = parseCmdLineOpt(argc, argv, "ehvVl", 0, &myRodsArgs);
     if (status != 0) {
@@ -43,7 +87,7 @@ main(int argc, char **argv)
        password = argv[ix];
     }
 
-    status = getRodsEnv (&myEnv);  /* Need to get irodsAuthFileName (if set) */
+    status = getRodsEnv (&myEnv);
     if (status < 0) {
        rodsLog (LOG_ERROR, "main: getRodsEnv error. status = %d",
 		status);
@@ -55,6 +99,83 @@ main(int argc, char **argv)
 	exit (0);
     }
 
+/*
+ Create ~/.irods/ if it does not exist
+ */
+    mkrodsdir(); 
+
+ /*
+    Check on the key Environment values, prompt and save
+    them if not already available.
+  */
+    if (myEnv.rodsHost == NULL || strlen(myEnv.rodsHost)==0) {
+       int i;
+       if (doingEnvFileUpdate==0) {
+	  doingEnvFileUpdate=1;
+	  printUpdateMsg();
+       }
+       printf("Enter the host name (DNS) of the server to connect to:");
+       memset(ttybuf, 0, TTYBUF_LEN);
+       fgets(ttybuf, TTYBUF_LEN, stdin);
+       rstrcat(updateText,"irodsHost ", UPDATE_TEXT_LEN);
+       rstrcat(updateText,ttybuf, UPDATE_TEXT_LEN);
+       i = strlen(ttybuf);
+       if (i>0) ttybuf[i-1]='\0'; /* chop off trailing \n */
+       strncpy(myEnv.rodsHost, ttybuf, NAME_LEN);
+    }
+    if (myEnv.rodsPort == 0) {
+       int i;
+       if (doingEnvFileUpdate==0) {
+	  doingEnvFileUpdate=1;
+	  printUpdateMsg();
+       }
+       printf("Enter the port number:");
+       memset(ttybuf, 0, TTYBUF_LEN);
+       fgets(ttybuf, TTYBUF_LEN, stdin);
+       rstrcat(updateText,"irodsPort ", UPDATE_TEXT_LEN);
+       rstrcat(updateText,ttybuf, UPDATE_TEXT_LEN);
+       i = strlen(ttybuf);
+       if (i>0) ttybuf[i-1]='\0';
+       myEnv.rodsPort = atoi(ttybuf);
+    }
+    if (myEnv.rodsUserName == NULL || strlen(myEnv.rodsUserName)==0) {
+       int i;
+       if (doingEnvFileUpdate==0) {
+	  doingEnvFileUpdate=1;
+	  printUpdateMsg();
+       }
+       printf("Enter your irods user name:");
+       memset(ttybuf, 0, TTYBUF_LEN);
+       fgets(ttybuf, TTYBUF_LEN, stdin);
+       rstrcat(updateText,"irodsUserName ", UPDATE_TEXT_LEN);
+       rstrcat(updateText,ttybuf, UPDATE_TEXT_LEN);
+       i = strlen(ttybuf);
+       if (i>0) ttybuf[i-1]='\0';
+       strncpy(myEnv.rodsUserName, ttybuf, NAME_LEN);
+    }
+    if (myEnv.rodsZone == NULL || strlen(myEnv.rodsZone)==0) {
+       int i;
+       if (doingEnvFileUpdate==0) {
+	  doingEnvFileUpdate=1;
+	  printUpdateMsg();
+       }
+       printf("Enter your irods zone:");
+       memset(ttybuf, 0, TTYBUF_LEN);
+       fgets(ttybuf, TTYBUF_LEN, stdin);
+       rstrcat(updateText,"irodsZone ", UPDATE_TEXT_LEN);
+       rstrcat(updateText,ttybuf, UPDATE_TEXT_LEN);
+       i = strlen(ttybuf);
+       if (i>0) ttybuf[i-1]='\0';
+       strncpy(myEnv.rodsZone, ttybuf, NAME_LEN);
+    }
+    if (doingEnvFileUpdate) {
+       printf("Those values will be added to your environment file (for use by\n");
+       printf("other i-commands) if the login succeeds.\n\n");
+    }
+
+    /*
+      Now, get the password
+     */
     doPassword=1;
 #if defined(GSI_AUTH)
     if (strncmp("GSI",myEnv.rodsAuthScheme,3)==0) {
@@ -101,6 +222,11 @@ main(int argc, char **argv)
     }
 
     rcDisconnect(Conn);
+
+    /* Save updates to .irodsEnv. */
+    if (doingEnvFileUpdate==1) {
+       appendRodsEnv(updateText);
+    }
 
     exit (0);
 }
