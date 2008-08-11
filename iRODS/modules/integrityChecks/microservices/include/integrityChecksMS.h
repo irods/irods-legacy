ac85498a4423ea99211e5e3418412d84bb601826
lindsey <susanunit@gmail.com>
2008-08-11 21:26:29 +0000

    - Added two microservices rules & one function
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1056 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/modules/integrityChecks/microservices/include/integrityChecksMS.h
+++ iRODS/modules/integrityChecks/microservices/include/integrityChecksMS.h
@@ -12,21 +12,16 @@
 #ifndef INTEGRITYCHECKS_H
 #define INTEGRITYCHECKS_H
 
-//#include "rods.h"
-//#include "rodsDef.h"
-//#include "miscUtil.h"
-//#include "apiHeaderAll.h"
-//#include "objMetaOpr.h"
-
 #include "apiHeaderAll.h"
 #include "objMetaOpr.h"
 #include "miscUtil.h"
 
-
 int msiHiThere (ruleExecInfo_t *rei);
 int msiCheckFilesizeRange (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3, msParam_t *mPout1, ruleExecInfo_t *rei);
 int msiCheckFileDatatypes (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPout1, ruleExecInfo_t *rei);
-int msiListCollection (msParam_t *mPin1, msParam_t *mPout1, ruleExecInfo_t *rei);
+int msiVerifyOwner (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPout1, msParam_t *mPout2, ruleExecInfo_t *rei);
+int msiVerifyACL (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPin3, msParam_t *mPin4, msParam_t *mPout1, 
+	msParam_t *mPout2, ruleExecInfo_t *rei);
 
 int hithere ();
 
