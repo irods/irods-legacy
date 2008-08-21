39fac71145c7ccff26045580b1c9d0e4456117b3
lindsey <susanunit@gmail.com>
2008-08-21 22:41:22 +0000

    - More working on ACL microservices
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1101 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/modules/integrityChecks/microservices/src/icutils.c
+++ iRODS/modules/integrityChecks/microservices/src/icutils.c
@@ -8,10 +8,13 @@ int msiListCollACL (msParam_t* mPin1, msParam_t* mPout1, msParam_t* mPout2, rule
 	genQueryInp_t	gqin;
 	genQueryOut_t*	gqout = NULL;
 	char* collname=NULL;
+	char* collid=NULL;
 	rsComm_t *rsComm;
 	bytesBuf_t* mybuf=NULL;
-	sqlResult_t* collUserName;
+	sqlResult_t* collAccessType;;
+	sqlResult_t* collCollId;
 	sqlResult_t* collAccessName;
+	sqlResult_t* collDataName;
 
 	RE_TEST_MACRO ("    Calling msiListCollACL")
 
@@ -27,16 +30,17 @@ int msiListCollACL (msParam_t* mPin1, msParam_t* mPout1, msParam_t* mPout2, rule
 	memset (mybuf, 0, sizeof (bytesBuf_t));
 	gqout = (genQueryOut_t *) malloc (sizeof (genQueryOut_t));
 	memset (gqout, 0, sizeof (genQueryOut_t));
-	clearGenQueryInp (&gqin);
-
+	memset (&gqin, 0, sizeof (genQueryInp_t));
+	//clearGenQueryInp (&gqin);
 	gqin.maxRows = MAX_SQL_ROWS;
-
-	addInxIval (&gqin.selectInp, COL_COLL_USER_NAME, 1);
-	addInxIval (&gqin.selectInp, COL_COLL_ACCESS_NAME, 1);
 
 	/* Currently necessary since other namespaces exist in the token table */
 	//snprintf (tmpstr, MAX_NAME_LEN, "='%s'", "access_type");
 	//addInxVal (&gqin.sqlCondInp, COL_COLL_TOKEN_NAMESPACE, tmpStr);
+
+	/* First get the collection id from the collection name - then using 
+		that info - and in the future chaining together these microservices -
+		we can get all the data objects access types */
 
 	collname = (char*) strdup (mPin1->inOutStruct);
 	if (collname == NULL) {
@@ -45,28 +49,59 @@ int msiListCollACL (msParam_t* mPin1, msParam_t* mPout1, msParam_t* mPout2, rule
 		snprintf (tmpstr, MAX_NAME_LEN, " = '%s'", collname);
 		addInxVal (&gqin.sqlCondInp, COL_COLL_NAME, tmpstr);
 	}
+	addInxIval (&gqin.selectInp, COL_D_COLL_ID, 1);
 
 	j = rsGenQuery (rsComm, &gqin, &gqout);
 	
 	if (j<0) {
-		appendToByteBuf (mybuf, "Some genQuery error\n");
+		appendToByteBuf (mybuf, "Coll ID Not found\n");
 	}
 	else if (j != CAT_NO_ROWS_FOUND) {
 
-		//printGenQueryOut(stderr, NULL, NULL, gqout);
-		collUserName = getSqlResultByInx (gqout, COL_COLL_USER_NAME);
-		collAccessName = getSqlResultByInx (gqout, COL_COLL_ACCESS_NAME);
+		printGenQueryOut(stderr, NULL, NULL, gqout);
 
-		for (i=0; i<gqout->rowCnt; i++) {
-			sprintf (tmpstr, "Coll user name:%s\taccess name:%s\n", &collUserName->value[collUserName->len *i], &collAccessName->value[collAccessName->len *i]);
-			appendToByteBuf (mybuf, tmpstr);
-		}
-		appendToByteBuf (mybuf, "Something was found\n");
+		/* don't we really just need the first element, since all the collection id's should be the same? */
+		collCollId = getSqlResultByInx (gqout, COL_D_COLL_ID);
+		collid = strdup (&collCollId->value[0]);
+		sprintf (tmpstr, "Collection ID:%s\n", &collCollId->value[0]);
+		appendToByteBuf (mybuf, tmpstr);
 
 	} else {
 
 		appendToByteBuf (mybuf, "Some unknown error\n");
 	}
+
+	/* Now do the query to get all the interesting information re: ACLS */
+	memset (&gqin, 0, sizeof (genQueryInp_t));
+	gqin.maxRows = MAX_SQL_ROWS;
+	
+	addInxIval (&gqin.selectInp, COL_DATA_NAME, 1);
+	addInxIval (&gqin.selectInp, COL_DATA_ACCESS_NAME, 1);
+	addInxIval (&gqin.selectInp, COL_DATA_ACCESS_TYPE, 1);
+	snprintf (tmpstr, MAX_NAME_LEN, " = '%s'", collid);
+	addInxVal (&gqin.sqlCondInp, COL_D_COLL_ID, tmpstr);
+
+	j = rsGenQuery (rsComm, &gqin, &gqout);
+
+	if (j<0) {
+		 appendToByteBuf (mybuf, "Second gen query bad\n");
+	} else if  (j != CAT_NO_ROWS_FOUND) {
+
+		collDataName = getSqlResultByInx (gqout, COL_DATA_NAME);
+		collAccessType = getSqlResultByInx (gqout, COL_DATA_ACCESS_TYPE);
+		collAccessName = getSqlResultByInx (gqout, COL_DATA_ACCESS_NAME);
+		fprintf (stderr, "rowCnt:%d\n", gqout->rowCnt);
+		for (i=0; i<gqout->rowCnt; i++) {
+			sprintf (tmpstr, "Data name:%s\tUser name:%s\tAccess Name:%s\n", 
+				&collDataName->value[collDataName->len *i], 
+				&collAccessType->value[collAccessType->len *i], 
+				&collAccessName->value[collAccessName->len *i]);
+			appendToByteBuf (mybuf, tmpstr);
+		}	
+
+		printGenQueryOut(stderr, NULL, NULL, gqout);
+	}
+	
 
 	fillBufLenInMsParam (mPout1, mybuf->len, mybuf);
 	fillIntInMsParam (mPout2, rei->status);
@@ -92,6 +127,7 @@ int msiListFields (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPout1, msPara
 	bytesBuf_t*	mybuf=NULL;
 	int i,j;
 	int	fieldid;
+	int debug=1;
 
 	RE_TEST_MACRO ("    Calling msiListFields")
 
@@ -103,18 +139,25 @@ int msiListFields (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPout1, msPara
 
 	rsComm = rei->rsComm;
 
-	/* init stuff */
-	memset (&gqin, 0, sizeof(genQueryInp_t));
+	/* init gqout, gqin & mybuf structs */
+	mybuf = (bytesBuf_t *) malloc(sizeof(bytesBuf_t));
+	memset (mybuf, 0, sizeof (bytesBuf_t));
+	gqout = (genQueryOut_t *) malloc (sizeof (genQueryOut_t));
+	memset (gqout, 0, sizeof (genQueryOut_t));
+	memset (&gqin, 0, sizeof (genQueryInp_t));
+
 	gqin.maxRows = MAX_SQL_ROWS;
-    mybuf = (bytesBuf_t *)malloc(sizeof(bytesBuf_t));
-    memset (mybuf, 0, sizeof (bytesBuf_t));
 
 	/* construct an SQL query from the parameter list */
 	collname = (char*) strdup (mPin1->inOutStruct);
 	fieldname = (char*) strdup (mPin2->inOutStruct);
-	fieldid = getAttrIdFromAttrName(fieldname);
+	if ((fieldid = getAttrIdFromAttrName(fieldname))==NO_COLUMN_NAME_FOUND) {
+		sprintf (tmpstr, "Field: %s not found in database", fieldname);
+		appendToByteBuf (mybuf, tmpstr);
+		return (-1);
+	}
 
-	rodsLog (LOG_ERROR, "fieldname: %s\tfieldid:%d", fieldname,fieldid);
+	if (debug) rodsLog (LOG_NOTICE, "fieldname: %s\tfieldid:%d", fieldname,fieldid);
 
 	/* this is the info we want returned from the query */
 	addInxIval (&gqin.selectInp, COL_DATA_NAME, 1);
@@ -122,13 +165,11 @@ int msiListFields (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPout1, msPara
 	snprintf (condStr, MAX_NAME_LEN, " = '%s'", collname);
 	addInxVal (&gqin.sqlCondInp, COL_COLL_NAME, condStr);
 
-	rodsLog (LOG_ERROR, "got here 1");
-
 	j = rsGenQuery (rsComm, &gqin, &gqout);
 
-	if (j != CAT_NO_ROWS_FOUND) {
-
-		rodsLog (LOG_ERROR, "got here 2");
+	if (j<0) {
+		appendToByteBuf (mybuf, "Gen Query Error returned badness");
+	} else if (j != CAT_NO_ROWS_FOUND) {
 
 		printGenQueryOut(stderr, NULL, NULL, gqout);
 
@@ -143,7 +184,7 @@ int msiListFields (msParam_t *mPin1, msParam_t *mPin2, msParam_t *mPout1, msPara
 			appendToByteBuf (mybuf, tmpstr);
 		}
 
-	} 
+	} else appendToByteBuf (mybuf, "No matching rows found");
 
 	fillBufLenInMsParam (mPout1, mybuf->len, mybuf);
 	fillIntInMsParam (mPout2, rei->status);
