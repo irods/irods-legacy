6cdfddbfcfb327de71142f8616acfd06077306a8
lindsey <susanunit@gmail.com>
2008-09-04 23:35:24 +0000

    - Skeleton for recursive 'ls' on collections - derived from Mike's rsRmColl routines.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1140 ea137bd6-aa86-47b6-b90d-b101d9f19a90

---
+++ iRODS/modules/integrityChecks/microservices/include/icCollUtil.h
@@ -0,0 +1,31 @@
+/*** Copyright (c), The Regents of the University of California            ***
+ *** For more information please refer to files in the COPYRIGHT directory ***/
+/* icCollUtil.h
+ */
+
+#ifndef _IC_COLL_UTIL
+#define _IC_COLL_UTIL
+
+
+#include "rods.h"
+#include "rcMisc.h"
+#include "rodsClient.h"
+#include "rsApiHandler.h"
+#include "objMetaOpr.h"
+
+/*
+#include "dataObjInpOut.h"
+#include "procApiRequest.h"
+#include "apiNumber.h"
+#include "initServer.h"
+*/
+
+/* prototype for the server handler */
+int icListColl (rsComm_t *rsComm, collInp_t *rmCollInp, collOprStat_t **collOprStat);
+int _icListColl (rsComm_t *rsComm, collInp_t *rmCollInp, collOprStat_t **collOprStat);
+int _icListCollRecur (rsComm_t *rsComm, collInp_t *rmCollInp, collOprStat_t **collOprStat);
+int _icPhyListColl (rsComm_t *rsComm, collInp_t *rmCollInp, dataObjInfo_t *dataObjInfo, collOprStat_t **collOprStat);
+
+//int svrUnregColl (rsComm_t *rsComm, collInp_t *rmCollInp);
+
+#endif	/* _IC_COLL_UTIL */
