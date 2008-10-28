d81966909bedbcb3789e13edf5f795978fbc7a91
mywan <mwan@diceresearch.org>
2008-10-28 16:42:57 +0000

    Made a number of changes to make the ibun command easier to use.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1332 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/icommands/src/ibun.c
+++ iRODS/clients/icommands/src/ibun.c
@@ -49,11 +49,13 @@ main(int argc, char **argv) {
         exit (1);
     }
 
+#if 0
     if (*argv[optind] != '/' || *argv[optind + 1] != '/') { 
 	rodsLog (LOG_ERROR,
 	 "Input path must be absolute");
 	exit (1);
     }
+#endif
 
     status = parseCmdLinePath (argc, argv, optind, &myEnv,
       UNKNOWN_OBJ_T, UNKNOWN_OBJ_T, 0, &rodsPathInp);
@@ -93,20 +95,47 @@ void
 usage ()
 {
    char *msgs[]={
-"Usage : ibun -x [-h] -R resource structFilePath",
+"Usage : ibun -x [-h] [-R resource] structFilePath",
 "               irodsCollection",
-"Usage : ibun -c [-hf] -R resource [-D dataType] structFilePath",
+"Usage : ibun -c [-hf] [-R resource] [-D dataType] structFilePath",
 "               irodsCollection",
 
 " ",
-"Bundle file operations",
+"Bundle file operations. This command allows structured files such as ",
+"tar files to be uploaded and downloaded to/from iRods.",
+" ",
+"A tar file containing many small files can be created with normal unix",
+"tar command on the client and then uploaded to the iRods server as a",
+"normal iRods file. The 'ibun -x' command can the be used to extract/untar",
+"the uploaded tar file. The extracted subfiles and subdirectories will",
+"appeared as normal iRods files and sub-collections. The 'ibun -c' command",
+"can be used to tar/bundle an iRods collection into a tar file.",
+" ",
+"For example, to upload a directory mydir to iRods:",
+" ",
+"    tar -chlf mydir.tar -C /x/y/z/mydir .",
+"    iput -Dtar mydir.tar .",
+"    ibun -x mydir.tar mydir", 
+" ",
+"Note the use of -C option with the tar command which will tar the",
+"content of mydir but without including the directory mydir in the paths.",
+"Also, the -Dtar option is needed with the iput command so that the dataType",
+"'tar file' is associated with mydir.tar. The 'ibun -x' command extracts the",  
+"tar file into the mydir collection. The mydir collection must either does",
+"not exist or is empty.",
+" ",
+"The following command bundles the iRods collection mydir into a tar file:",
+" ",
+"    ibun -cDtar mydir1.tar mydir", 
 " ",
 " ",
 "Options are:",
 " -R  resource - specifies the resource to store to. This is optional",
 "     in your environment",
 " -D  dataType - the struct file data type. Valid only if the struct file",
-"     does not exist.",
+"     does not exist. Currently only one dataType - 't' which specifies",
+"     a tar file type is supported. If -D is not specified, the default is",
+"     a tar file type",
 " -x  extract the structFile and register the extracted files and directories",
 "     under the input irodsCollection", 
 " -c  bundle the files and sub-collection underneath the input irodsCollection",
