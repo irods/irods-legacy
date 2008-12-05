4b486366b631adb6fbc1c05538caf7697330e9c3
iktome <iktome@diceresearch.org>
2008-12-05 00:19:56 +0000

    typo in comments
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1449 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/core/src/bunUtil.c
+++ iRODS/lib/core/src/bunUtil.c
@@ -1,4 +1,4 @@
-/*** Copyright (c), The BunOprents of the University of California            ***
+/*** Copyright (c), The Regents of the University of California            ***
  *** For more information please refer to files in the COPYRIGHT directory ***/
 #include "rodsPath.h"
 #include "rodsErrorTable.h"
