8867f28c633f1eb6bf3224a59ff58ad1c819d280
Wayne-Schroeder <schroeder@diceresearch.org>
2009-02-24 19:37:35 +0000

    Two new tables for Resource Monitor System
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1545 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/icat/src/icatSysTables.sql
+++ iRODS/server/icat/src/icatSysTables.sql
@@ -281,6 +281,26 @@ create table R_OBJT_AUDIT
    modify_ts           varchar(32)
  );
 
+create table R_SERVER_LOAD
+(
+    host_name  varchar(250) not null,
+    resc_name  varchar(250) not null,
+    cpu_used   INTEGER,
+    mem_used   INTEGER,
+    swap_used  INTEGER,
+    runq_load  INTEGER,
+    disk_space INTEGER,
+    net_input  INTEGER,
+    net_output INTEGER,
+    create_ts  varchar(32)
+ );
+
+create table R_SERVER_LOAD_DIGEST
+(
+    resc_name   varchar(250) not null,
+    load_factor INTEGER,
+    create_ts varchar(32)
+);
 
 
 create sequence R_ObjectId increment by 1 start with 10000;
