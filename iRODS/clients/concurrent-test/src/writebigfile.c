c109207bda68966c4c4d634c3172e5f30a9d108b
Wayne-Schroeder <schroeder@diceresearch.org>
2009-04-02 22:32:32 +0000

    Allow an optional command line argument defining how large of a file to make.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1642 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/concurrent-test/src/writebigfile.c
+++ iRODS/clients/concurrent-test/src/writebigfile.c
@@ -5,14 +5,21 @@ int main (int argc, char** argv) {
 
 	FILE* fp;
 	int	i;
-	char x[256];
+	char x[1024];
+	int limit=25000;
+
+	if (argc > 1) {
+	   int j;
+	   j = atoi(argv[1]);
+	   if (j > 0) limit=j;
+	}
 
 	fp = fopen ("bigfile", "w");
 
-	for (i=0; i<256; i++)
+	for (i=0; i<1024; i++)
 		x[i]=(char) i;
 
-	for (i=0; i<100000; i++)
+	for (i=0; i<limit; i++)
 		fwrite(x, sizeof(x[0]), sizeof(x)/sizeof(x[0]), fp);
 
 	fclose (fp);
