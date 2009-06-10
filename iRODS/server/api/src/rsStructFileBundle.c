42001387cfd3bdf37808dd01cff97de761214031
mywan <mwan@diceresearch.org>
2009-06-10 01:00:22 +0000

    redo logical bundle for executable tar
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1789 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/api/src/rsStructFileBundle.c
+++ iRODS/server/api/src/rsStructFileBundle.c
@@ -17,7 +17,16 @@ structFileExtAndRegInp_t *structFileBundleInp)
     int status;
     dataObjInp_t dataObjInp;
     openedDataObjInp_t dataObjCloseInp;
+#if 0
     structFileOprInp_t structFileOprInp;
+#else
+    collInp_t collInp;
+    collEnt_t *collEnt;
+    int handleInx;
+    int collLen;
+    char phyBunDir[MAX_NAME_LEN];
+    char tmpPath[MAX_NAME_LEN];
+#endif
     chkObjPermAndStat_t chkObjPermAndStatInp;
     int l1descInx;
     int remoteFlag;
@@ -47,17 +56,8 @@ structFileExtAndRegInp_t *structFileBundleInp)
     l1descInx = _rsDataObjOpen (rsComm, &dataObjInp);
 
     if (l1descInx < 0) {
-#if 0
-	if (getValByKey (&dataObjInp.condInput, DEST_RESC_NAME_KW) == NULL &&
-	  getValByKey (&dataObjInp.condInput, DEF_RESC_NAME_KW) == NUL) {
-	    return SYS_CACHE_STRUCT_FILE_RESC_ERR;
-	} else {
-#endif
 	    rmKeyVal (&dataObjInp.condInput, NO_OPEN_FLAG_KW);
 	    l1descInx = rsDataObjCreate (rsComm, &dataObjInp);
-#if 0
-	}
-#endif
 
 	if (l1descInx < 0) {
             rodsLog (LOG_ERROR,
@@ -67,6 +67,7 @@ structFileExtAndRegInp_t *structFileBundleInp)
 	}
     }
 
+#if 0
     status = initStructFileOprInp (rsComm, &structFileOprInp, 
       structFileBundleInp, L1desc[l1descInx].dataObjInfo);
 
@@ -79,6 +80,7 @@ structFileExtAndRegInp_t *structFileBundleInp)
         rsDataObjClose (rsComm, &dataObjCloseInp);
         return (status);
     }
+#endif
 
     memset (&chkObjPermAndStatInp, 0, sizeof (chkObjPermAndStatInp));
     rstrcpy (chkObjPermAndStatInp.objPath, 
@@ -98,6 +100,7 @@ structFileExtAndRegInp_t *structFileBundleInp)
         return (status);
     }
 
+#if 0
     structFileOprInp.oprType = NO_REG_COLL_INFO | LOGICAL_BUNDLE;
     structFileOprInp.specColl->cacheDirty = 1;
 
@@ -110,6 +113,63 @@ structFileExtAndRegInp_t *structFileBundleInp)
     } else {
 	/* mark it was written so the size would be adjusted */
 	L1desc[l1descInx].bytesWritten = 1;
+    }
+#endif
+
+    createPhyBundleDir (rsComm, L1desc[l1descInx].dataObjInfo->filePath,
+      phyBunDir);
+
+    bzero (&collInp, sizeof (collInp));
+    rstrcpy (collInp.collName, structFileBundleInp->collection, MAX_NAME_LEN);
+    collInp.flags = RECUR_QUERY_FG | VERY_LONG_METADATA_FG |
+      NO_TRIM_REPL_FG;
+    handleInx = rsOpenCollection (rsComm, &collInp);
+    if (handleInx < 0) {
+        rodsLog (LOG_ERROR,
+          "rsStructFileBundle: rsOpenCollection of %s error. status = %d",
+          collInp.collName, handleInx);
+        return (handleInx);
+    }
+
+    collLen = strlen (collInp.collName) + 1;
+
+    while ((status = rsReadCollection (rsComm, &handleInx, &collEnt)) >= 0) {
+        if (collEnt->objType == DATA_OBJ_T) {
+            if (collEnt->collName[collLen] == '\0') {
+                snprintf (tmpPath, MAX_NAME_LEN, "%s/%s",
+                  phyBunDir, collEnt->dataName);
+            } else {
+                snprintf (tmpPath, MAX_NAME_LEN, "%s/%s/%s",
+                  phyBunDir, collEnt->collName + collLen, collEnt->dataName);
+            }
+	    mkdirR (phyBunDir, collEnt->collName, DEFAULT_DIR_MODE);
+            /* add a link */
+            status = link (collEnt->phyPath, tmpPath);
+            if (status < 0) {
+                rodsLog (LOG_ERROR,
+                  "rsStructFileBundle: link error %s to %s. errno = %d",
+                  collEnt->phyPath, tmpPath, errno);
+                return (UNIX_FILE_LINK_ERR - errno);
+            }
+        } else {        /* a collection */
+            snprintf (tmpPath, MAX_NAME_LEN, "%s/%s",
+              phyBunDir, collEnt->collName + collLen);
+            mkdirR (phyBunDir, tmpPath, DEFAULT_DIR_MODE);
+	}
+    }
+    status = phyBundle (rsComm, L1desc[l1descInx].dataObjInfo, phyBunDir,
+      collInp.collName);
+    if (status < 0) {
+        rodsLog (LOG_ERROR,
+          "rsStructFileBundle: phyBundle of %s error. stat = %d",
+          L1desc[l1descInx].dataObjInfo->objPath, status);
+        rmFilesInUnixDir (phyBunDir);
+        rmdir (phyBunDir);
+	L1desc[l1descInx].bytesWritten = 0;
+        return status;
+    } else {
+        /* mark it was written so the size would be adjusted */
+        L1desc[l1descInx].bytesWritten = 1;
     }
 
     dataObjCloseInp.l1descInx = l1descInx;
