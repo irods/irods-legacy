865437d99578a57a127c1ca10b7c6f844b936f25
Wayne-Schroeder <schroeder@diceresearch.org>
2009-08-12 17:48:27 +0000

    Correction to previous commit, need to include the create-timestamp column.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@1998 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/icat/src/psg-patch-v2.1tov2.2.sql
+++ iRODS/server/icat/src/psg-patch-v2.1tov2.2.sql
@@ -5,7 +5,8 @@ alter table R_RESC_MAIN add column resc_status varchar(32);
 create table R_USER_AUTH
 (
    user_id             bigint not null,
-   user_auth_name      varchar(1000)
+   user_auth_name      varchar(1000),
+   create_ts varchar(32)
 );
 
 insert into R_USER_AUTH ( user_id, user_auth_name ) select user_id, user_distin_name from r_user_main where user_distin_name <> '';
