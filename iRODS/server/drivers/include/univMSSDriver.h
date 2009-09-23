73ef46f2a185c24ede8a968065e4c5ac2a243529
nief <nief@cc.in2p3.fr>
2009-09-23 14:38:00 +0000

    header file for the universal MSS driver.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@2099 ea137bd6-aa86-47b6-b90d-b101d9f19a90

---
+++ iRODS/server/drivers/include/univMSSDriver.h
@@ -0,0 +1,35 @@
+/*** Copyright (c), The Regents of the University of California            ***
+ *** For more information please refer to files in the COPYRIGHT directory ***/
+/* The module is written by Jean-Yves Nief of CCIN2P3 */
+
+/* univMSSDriver.h - header file for univMSSDriver.c
+ */
+
+
+#ifndef UNIV_MSS_DRIVER_H
+#define UNIV_MSS_DRIVER_H
+
+#define UNIV_MSS_INTERF_SCRIPT "univMSSInterface.sh"
+
+#include "rods.h"
+#include "rcConnect.h"
+#include "msParam.h"
+
+int
+univMSSFileUnlink (rsComm_t *rsComm, char *filename);
+int
+univMSSFileMkdir (rsComm_t *rsComm, char *filename, int mode);
+int
+univMSSFileChmod (rsComm_t *rsComm, char *filename, int mode);
+int
+univMSSStageToCache (rsComm_t *rsComm, fileDriverType_t cacheFileType,
+int mode, int flags, char *filename,
+char *cacheFilename, rodsLong_t dataSize,
+keyValPair_t *condInput);
+int
+univMSSSyncToArch (rsComm_t *rsComm, fileDriverType_t cacheFileType,
+int mode, int flags, char *filename,
+char *cacheFilename,  rodsLong_t dataSize,
+keyValPair_t *condInput);
+
+#endif	/* UNIV_MSS_DRIVER_H */
