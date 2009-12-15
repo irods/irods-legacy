7deaadcefe4849fa8eb1b70dc0a8df6b4b386fbb
Wayne-Schroeder <schroeder@diceresearch.org>
2009-12-15 22:03:25 +0000

    Add another test, this one for finding data-object AVUs and ordering them by their values.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@2282 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/icommands/test/iTestGenQuery.c
+++ iRODS/clients/icommands/test/iTestGenQuery.c
@@ -268,8 +268,71 @@ doTest4(rcComm_t *Conn,
        printf("genQueryOut->totalRowCount=%d\n", genQueryOut->totalRowCount);
 
        printGenQOut(genQueryOut);
+
     }
 
+    return(0);
+}
+
+/*
+Find data-objects with AVUs of the input name, ordered by the value
+of that AVU.
+ */
+int
+doTest5(rcComm_t *Conn,
+	char *AvuName, char *order) {
+    genQueryInp_t genQueryInp;
+    genQueryOut_t *genQueryOut;
+    int status;
+    char condition1[MAX_NAME_LEN];
+
+    printf("dotest5\n");
+    rodsLogSqlReq(1);
+
+    memset (&genQueryInp, 0, sizeof (genQueryInp));
+
+    snprintf (condition1, MAX_NAME_LEN, "='%s'", AvuName);
+    addInxVal (&genQueryInp.sqlCondInp, COL_META_DATA_ATTR_NAME, condition1);
+    printf("condition1 %s\n", condition1);
+
+    addInxIval (&genQueryInp.selectInp, COL_DATA_NAME, 0);
+    addInxIval (&genQueryInp.selectInp, COL_COLL_NAME, 0);
+
+    if (order == NULL) {
+	  addInxIval (&genQueryInp.selectInp, COL_META_DATA_ATTR_VALUE, 
+		      ORDER_BY_DESC);    /* regular, descending order */
+    }
+    else {
+       if (strcmp(order, "up")==0) {     /* ascending order */
+	  addInxIval (&genQueryInp.selectInp, COL_META_DATA_ATTR_VALUE, 
+		      ORDER_BY);
+       }
+       else {                           /* regular, descending order */
+	  addInxIval (&genQueryInp.selectInp, COL_META_DATA_ATTR_VALUE, 
+		      ORDER_BY_DESC);
+       }
+    }
+
+    genQueryInp.maxRows = 2;       /* get two rows to better show the results,
+				      but could just get 1 */
+
+    status =  rcGenQuery (Conn, &genQueryInp, &genQueryOut); 
+
+    printf("GenQuery status=%d\n",status);
+    if (status) printError(Conn, status, "rcGenQuery");
+
+    if (status == 0) {
+       printf("genQueryOut->totalRowCount=%d\n", genQueryOut->totalRowCount);
+
+       printGenQOut(genQueryOut);
+
+       if (genQueryOut->continueInx > 0) { /* there are more rows available */
+	  genQueryInp.continueInx=genQueryOut->continueInx; 
+	  genQueryInp.maxRows = 0; /* just close it out */
+	  status = rcGenQuery (Conn, &genQueryInp, &genQueryOut); 
+	  printf ("close out rcGenQuery status=%d\n",status);
+       }
+    }
     return(0);
 }
 
@@ -353,6 +416,9 @@ main(int argc, char **argv)
        }
        if (strcmp(argv[1],"DN")==0) {
 	  doTest4(Conn, argv[2], argv[3]);
+       }
+       if (strcmp(argv[1],"AVU")==0) {
+	  doTest5(Conn, argv[2], argv[3]);
        }
     }
 
