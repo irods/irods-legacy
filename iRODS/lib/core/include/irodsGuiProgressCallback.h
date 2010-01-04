e8ef95c133ce818bbda6b1b03c6d7c971e200a30
bzhu <zhu@sciences.sdsu.edu>
2010-01-04 20:57:46 +0000

    The callback is used by iRODS GUI clients to show job progress.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@2312 ea137bd6-aa86-47b6-b90d-b101d9f19a90

---
+++ iRODS/lib/core/include/irodsGuiProgressCallback.h
@@ -0,0 +1,14 @@
+#ifndef _iRODSGuiProgressCallback_h_
+#define _iRODSGuiProgressCallback_h_
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+typedef void (*irodsGuiProgressCallbak)(char *current_file, float curpt);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
