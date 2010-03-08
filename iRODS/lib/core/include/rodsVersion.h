bcf07b17ef6166417a4295950119aa12751b796b
Wayne-Schroeder <schroeder@diceresearch.org>
2010-03-08 22:52:05 +0000

    Update iRODS version and release date in preparation for the release.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@2492 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/core/include/rodsVersion.h
+++ iRODS/lib/core/include/rodsVersion.h
@@ -9,9 +9,9 @@
 #ifndef RODS_VERSION_H
 #define RODS_VERSION_H
 
-#define RODS_REL_VERSION	"rods2.2"
+#define RODS_REL_VERSION	"rods2.3"
 #define RODS_API_VERSION	"d"
-#define RODS_RELEASE_DATE       "Oct 2009"
+#define RODS_RELEASE_DATE       "March 2010"
 
 
 #endif	/* RODS_VERSION_H */
