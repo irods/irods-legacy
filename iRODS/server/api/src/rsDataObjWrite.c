ba8bc3932eff42c42d6d3f3a4cadeeadbf151196
mywan <mwan@diceresearch.org>
2010-03-18 15:58:33 +0000

    minor change
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@2537 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/api/src/rsDataObjWrite.c
+++ iRODS/server/api/src/rsDataObjWrite.c
@@ -25,7 +25,6 @@ bytesBuf_t *dataObjWriteInpBBuf)
 	  l1descInx);
 	return (SYS_FILE_DESC_OUT_OF_RANGE);
     }
-
     if (L1desc[l1descInx].inuseFlag != FD_INUSE) return BAD_INPUT_DESC_INDEX;
     if (L1desc[l1descInx].remoteZoneHost != NULL) {
         /* cross zone operation */
