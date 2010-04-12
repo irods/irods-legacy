79a16bf893e60985255de84d508a98b5fe51a7ee
Wayne-Schroeder <schroeder@diceresearch.org>
2010-04-12 20:25:31 +0000

    Add and update tables for holding the rules in the ICAT.
    Update the columns in R_RULE_MAIN to match the current definitions
    (like those in the 2.2 to 2.3 patch files), and add the R_RULE_DVM and
    R_RULE_FNM (as per the patch files too).  I had forgotten to go back
    to the icatSysTables.sql and make these more recent changes (since
    these tables were not used in 2.3).
    
    We will need to account for this for the patch files for the next
    release, as sites who ran the patch will have the right tables but
    those who installed 2.3 from scratch will need to have them dropped
    and recreated.
    
    
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@2601 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/icat/src/icatSysTables.sql
+++ iRODS/server/icat/src/icatSysTables.sql
@@ -174,20 +174,53 @@ create table R_TOKN_MAIN
 
 create table R_RULE_MAIN
  (
-   rule_id             bigint not null,
-   rule_name           varchar(2700) not null,
-   rule_seq_num        bigint,
-   object_type_name    varchar(250) not null,
-   rule_event          varchar(2700) not null,
-   rule_condition      varchar(2700),
-   rule_body           varchar(2700) not null,
-   rulexec_type_name   varchar(250) not null,
-   rule_owner_name     varchar(250) not null,
-   rule_owner_zone     varchar(250) not null,
-   r_comment           varchar(1000),
-   create_ts           varchar(32),
-   modify_ts           varchar(32)
+   rule_id bigint not null,
+   rule_version varchar(250) DEFAULT '0',
+   rule_base_name varchar(250) not null,
+   rule_name varchar(250) not null DEFAULT 'null',
+   rule_event varchar(2700) not null,
+   rule_condition varchar(2700),
+   rule_body varchar(2700) not null,
+   rule_recovery varchar(2700) not null,
+   rule_status INTEGER DEFAULT 1,
+   rule_owner_name varchar(250) not null,
+   rule_owner_zone varchar(250) not null,
+   r_comment varchar(1000),
+   create_ts varchar(32) ,
+   modify_ts varchar(32)
  );
+
+create table R_RULE_DVM
+(
+   dvm_id bigint not null,
+   dvm_version varchar(250) DEFAULT '0',
+   dvm_base_name varchar(250) not null,
+   dvm_ext_var_name varchar(250) not null,
+   dvm_condition varchar(2700),
+   dvm_int_map_path varchar(2700) not null,
+   dvm_status INTEGER DEFAULT 1,
+   dvm_owner_name varchar(250) not null,
+   dvm_owner_zone varchar(250) not null,
+   r_comment varchar(1000),
+   create_ts varchar(32) ,
+   modify_ts varchar(32)
+);
+
+create table R_RULE_FNM
+(
+   fnm_id bigint not null,
+   fnm_version varchar(250) DEFAULT '0',
+   fnm_base_name varchar(250) not null,
+   fnm_ext_func_name varchar(250) not null,
+   fnm_int_func_name varchar(2700) not null,
+   fnm_status INTEGER DEFAULT 1,
+   fnm_owner_name varchar(250) not null,
+   fnm_owner_zone varchar(250) not null,
+   r_comment varchar(1000),
+   create_ts varchar(32) ,
+   modify_ts varchar(32)
+);
+
 
 create table R_RULE_EXEC
  (
