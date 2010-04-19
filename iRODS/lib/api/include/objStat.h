ffe93ac6786d13b3dd369b262ebd7c93c4819ffc
mywan <mwan@diceresearch.org>
2010-04-19 21:18:04 +0000

    Add comment to rcObjStat
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@2632 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/api/include/objStat.h
+++ iRODS/lib/api/include/objStat.h
@@ -81,6 +81,23 @@ specCollCache_t **specCollCache, keyValPair_t *condInput);
 #endif
 
 /* prototype for the client call */
+/* rcObjStat - get the stat of an object specified by dataObjInp->objPath.
+ * input:  dataObjInp 
+ *
+ * output: rodsObjStatOut
+ *   The objType can be COLL_OBJ_T (collection), DATA_OBJ_T (data object) 
+ *   or UNKNOWN_OBJ_T (object does not exist).  
+ *   If "specColl" is not NULL, the input objPath is a Special Collection or 
+ *   in a Special Collection. If objType is UNKNOWN_OBJ_T and "specColl" 
+ *   is not NULL, the object does not exist but the objPath is in a 
+ *   Special Collection.
+ *   Important items in the specColl_t are:
+ *     collClass - can be STRUCT_FILE_COLL (mounted structured file),
+ *     MOUNTED_COLL (mounted collection) or LINKED_COLL (linked collection).
+ *     objPath - If collClass is LINKED_COLL, this is the translated path
+ *     for the input objPath. The client should use this path instead of
+ *     the input "objPath" for further metadata query.
+ */
 int
 rcObjStat (rcComm_t *conn, dataObjInp_t *dataObjInp, 
 rodsObjStat_t **rodsObjStatOut);
