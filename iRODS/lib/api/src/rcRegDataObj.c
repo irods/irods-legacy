10f1b41853985d9f629de55c8995bdb4216d527b
mywan <mwan@diceresearch.org>
2010-07-12 19:19:26 +0000

    fix a small mem leak in rcRegReplica and rcRegDataObj.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@3088 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/api/src/rcRegDataObj.c
+++ iRODS/lib/api/src/rcRegDataObj.c
@@ -8,8 +8,22 @@ rcRegDataObj (rcComm_t *conn, dataObjInfo_t *dataObjInfo,
 dataObjInfo_t **outDataObjInfo)
 {
     int status;
+    rescInfo_t *srcRescInfo;
+    dataObjInfo_t *srcNext;
+
+    /* don't sent rescInfo and next */
+    srcRescInfo = dataObjInfo->rescInfo;
+    srcNext = dataObjInfo->next;
+    dataObjInfo->rescInfo = NULL;
+    dataObjInfo->next = NULL;
+
     status = procApiRequest (conn, REG_DATA_OBJ_AN, dataObjInfo, NULL, 
         (void **) outDataObjInfo, NULL);
+
+    /* restore */
+    dataObjInfo->rescInfo = srcRescInfo;
+    dataObjInfo->next = srcNext;
+
     /* cleanup fake pointers */
     if (status >= 0 && *outDataObjInfo != NULL) {
 	if ((*outDataObjInfo)->rescInfo != NULL) {
