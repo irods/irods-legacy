e42fa1933bc3565f2d9445d7355362713ee6c820
adetorcy <adetorcy@email.unc.edu>
2010-07-15 04:13:05 +0000

    Added example rules
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@3117 ea137bd6-aa86-47b6-b90d-b101d9f19a90

---
+++ iRODS/modules/ERA/test/isCollClean.r
@@ -0,0 +1,43 @@
+isCollClean
+# Needs to be slightly modified once in .ir form to append '%' to collname in 2d condition
+# Final assign() statement also needs to be manually fixed, see comment
+{
+## 1st query to get objects directly in the collection
+# Add select field(s)
+	msiAddSelectFieldToGenQuery(DATA_ID, COUNT, *GenQInp);
+# Add condition(s)
+	msiAddConditionToGenQuery(COLL_NAME, "=", *collection, *GenQInp);
+	msiAddConditionToGenQuery(META_DATA_ATTR_NAME, "like", "INFECTED%", *GenQInp);
+# Run query
+	msiExecGenQuery(*GenQInp, *GenQOut);
+# Extract and print out results, must be inside loop to work
+  	foreach (*GenQOut)
+	{
+		msiGetValByKey(*GenQOut, "DATA_ID", *objCount);
+	}
+	
+
+## 2d query to get objects in subcollections
+# Add select field(s)
+	msiAddSelectFieldToGenQuery(DATA_ID, COUNT, *GenQInp1);
+# Add condition(s)
+	msiAddConditionToGenQuery(COLL_NAME, "like", *collection, *GenQInp1);
+	msiAddConditionToGenQuery(META_DATA_ATTR_NAME, "like", "INFECTED%", *GenQInp1);
+# Run query
+	msiExecGenQuery(*GenQInp1, *GenQOut1);
+# Extract and print out results
+  	foreach (*GenQOut1)
+	{
+		msiGetValByKey(*GenQOut1, "DATA_ID", *objCount1);
+	}
+	
+## Add and print out total
+##	assign(*count, *objCount + *objCount1 );  
+##	to make it through rulegen
+	assign(*count, *objCount);
+#	writePosInt(stdout, *count);
+	writeLine(stdout,*count);
+
+}
+INPUT *collection = /tempZone/home/rods
+OUTPUT ruleExecOut
