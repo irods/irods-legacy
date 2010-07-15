125b93cd80f5535f8a8682e37430b00f21017593
Wayne-Schroeder <schroeder@diceresearch.org>
2010-07-15 23:11:39 +0000

    Add new indexes and new or changed rule tables
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@3126 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/icat/patches/psg-patch-v2.3tov2.4.sql
+++ iRODS/server/icat/patches/psg-patch-v2.3tov2.4.sql
@@ -1,12 +1,116 @@
 -- run these SQL statements, using the postgresql client psql 
 --
 
-ALTER TABLE r_resc_group ADD COLUMN resc_group_id bigint;
-ALTER TABLE r_resc_group ALTER COLUMN resc_group_id SET STORAGE PLAIN;
+ALTER TABLE R_RESC_GROUP ADD COLUMN resc_group_id bigint;
+ALTER TABLE R_RESC_GROUP ALTER COLUMN resc_group_id SET STORAGE PLAIN;
 
 -- Should generate the id for the existing resc_group ...
-UPDATE r_resc_group SET resc_group_id=nextval('r_objectid') WHERE resc_group_id IS NULL;
+UPDATE R_RESC_GROUP SET resc_group_id=nextval('r_objectid') WHERE resc_group_id IS NULL;
 
-ALTER TABLE r_resc_group ALTER COLUMN resc_group_id SET NOT NULL;
+ALTER TABLE R_RESC_GROUP ALTER COLUMN resc_group_id SET NOT NULL;
 
+--- Changes to the Rule tables
 
+DROP TABLE R_RULE_MAIN;
+DROP TABLE R_RULE_EXEC;
+
+create table R_RULE_MAIN
+ (
+   rule_id             bigint not null,
+   rule_version        varchar(250) DEFAULT '0',
+   rule_base_name      varchar(250) not null,
+   rule_name           varchar(2700) not null,
+   rule_event          varchar(2700) not null,
+   rule_condition      varchar(2700),
+   rule_body           varchar(2700) not null,
+   rule_recovery       varchar(2700) not null,
+   rule_status         bigint DEFAULT 1,
+   rule_owner_name     varchar(250) not null,
+   rule_owner_zone     varchar(250) not null,
+   rule_descr_1        varchar(2700),
+   rule_descr_2        varchar(2700),
+   input_params        varchar(2700),
+   output_params       varchar(2700),
+   dollar_vars         varchar(2700),
+   icat_elements       varchar(2700),
+   sideeffects         varchar(2700),
+   r_comment           varchar(1000),
+   create_ts           varchar(32),
+   modify_ts           varchar(32)
+ );
+
+create table R_RULE_BASE_MAP
+ (
+   map_version varchar(250) DEFAULT '0',
+   map_base_name varchar(250) not null,
+   map_priority INTEGER not null,
+   rule_id bigint not null,
+   map_owner_name varchar(250) not null,
+   map_owner_zone varchar(250) not null,
+   r_comment varchar(1000),
+   create_ts varchar(32) ,
+   modify_ts varchar(32)
+ );
+
+create table R_RULE_DVM
+(
+   dvm_id bigint not null,
+   dvm_version varchar(250) DEFAULT '0',
+   dvm_base_name varchar(250) not null,
+   dvm_ext_var_name varchar(250) not null,
+   dvm_condition varchar(2700),
+   dvm_int_map_path varchar(2700) not null,
+   dvm_status INTEGER DEFAULT 1,
+   dvm_owner_name varchar(250) not null,
+   dvm_owner_zone varchar(250) not null,
+   r_comment varchar(1000),
+   create_ts varchar(32) ,
+   modify_ts varchar(32)
+);
+
+create table R_RULE_FNM
+(
+   fnm_id bigint not null,
+   fnm_version varchar(250) DEFAULT '0',
+   fnm_base_name varchar(250) not null,
+   fnm_ext_func_name varchar(250) not null,
+   fnm_int_func_name varchar(2700) not null,
+   fnm_status INTEGER DEFAULT 1,
+   fnm_owner_name varchar(250) not null,
+   fnm_owner_zone varchar(250) not null,
+   r_comment varchar(1000),
+   create_ts varchar(32) ,
+   modify_ts varchar(32)
+);
+
+create table R_RULE_EXEC
+ (
+   rule_exec_id        bigint not null,
+   rule_name           varchar(2700) not null,
+   rei_file_path       varchar(2700),
+   user_name           varchar(250),
+   exe_address         varchar(250),
+   exe_time            varchar(32),
+   exe_frequency       varchar(250),
+   priority            varchar(32),
+   estimated_exe_time  varchar(32),
+   notification_addr   varchar(250),
+   last_exe_time       varchar(32),
+   exe_status          varchar(32),
+   create_ts           varchar(32),
+   modify_ts           varchar(32)
+ );
+
+--- New indexes
+
+create index idx_data_main3 on R_DATA_MAIN (coll_id);
+create index idx_data_main4 on R_DATA_MAIN (data_name);
+create index idx_meta_main2 on R_META_MAIN (meta_attr_name);
+create index idx_meta_main3 on R_META_MAIN (meta_attr_value);
+create index idx_meta_main4 on R_META_MAIN (meta_attr_unit);
+create index idx_objt_metamap2 on R_OBJT_METAMAP (object_id);
+create index idx_objt_metamap3 on R_OBJT_METAMAP (meta_id);
+create index idx_tokn_main1 on R_TOKN_MAIN (token_id);
+create index idx_tokn_main2 on R_TOKN_MAIN (token_name);
+create index idx_tokn_main3 on R_TOKN_MAIN (token_value);
+create index idx_tokn_main4 on R_TOKN_MAIN (token_namespace);
