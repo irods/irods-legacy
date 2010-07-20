da679e175d3036a6ae73cf2f97dd6e1eaf48fcd9
mywan <mwan@diceresearch.org>
2010-07-20 21:12:21 +0000

    change DEF_NUM_RE_PROCS to 1
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@3156 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/core/include/objMetaOpr.h
+++ iRODS/server/core/include/objMetaOpr.h
@@ -32,7 +32,7 @@
 #define MAX_ELAPSE_TIME 1800 /* max time in seconds above which the load info is considered to be out of date */
 
 #define MAX_RE_PROCS	4
-#define DEF_NUM_RE_PROCS	0
+#define DEF_NUM_RE_PROCS	1
 
 typedef enum {
     RE_PROC_IDLE,
