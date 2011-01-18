2e44b98ad68367d8606b8ae902c154f053fdc3d1
michael-conway <michael.c.conway@gmail.com>
2011-01-18 15:57:18 +0000

    Bug 119 - add enhanced streaming from remote exec of comds
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@3747 ea137bd6-aa86-47b6-b90d-b101d9f19a90

---
+++ iRODS/jargon/src/api/org/irods/jargon/core/connection/IRODSServerProperties.java
@@ -0,0 +1,135 @@
+/**
+ *
+ */
+package org.irods.jargon.core.connection;
+
+import java.util.Date;
+
+/**
+ * Immutable information on an IRODS Server that a connection is connected to.
+ * 
+ * @author Mike Conway - DICE (www.irods.org)
+ * 
+ */
+public final class IRODSServerProperties {
+
+	public static final String JARGON_VERSION = "2.2";
+
+	public enum IcatEnabled {
+		ICAT_ENABLED, NO_ICAT
+	}
+
+	private final Date initializeDate = new Date();
+	private final IcatEnabled icatEnabled;
+	private final int serverBootTime;
+	private final String relVersion;
+	private final String apiVersion;
+	private final String rodsZone;
+
+	public static IRODSServerProperties instance(final IcatEnabled icatEnabled,
+			final int serverBootTime, final String relVersion,
+			final String apiVersion, final String rodsZone) {
+		return new IRODSServerProperties(icatEnabled, serverBootTime,
+				relVersion, apiVersion, rodsZone);
+	}
+
+	private IRODSServerProperties(final IcatEnabled icatEnabled,
+			final int serverBootTime, final String relVersion,
+			final String apiVersion, final String rodsZone) {
+		super();
+		this.icatEnabled = icatEnabled;
+		this.serverBootTime = serverBootTime;
+		this.relVersion = relVersion;
+		this.apiVersion = apiVersion;
+		this.rodsZone = rodsZone;
+	}
+
+	public Date getInitializeDate() {
+		return initializeDate;
+	}
+
+	public static String getJargonVersion() {
+		return JARGON_VERSION;
+	}
+
+	public IcatEnabled getIcatEnabled() {
+		return icatEnabled;
+	}
+
+	public int getServerBootTime() {
+		return serverBootTime;
+	}
+
+	public String getRelVersion() {
+		return relVersion;
+	}
+
+	public String getApiVersion() {
+		return apiVersion;
+	}
+
+	public String getRodsZone() {
+		return rodsZone;
+	}
+
+	/**
+	 * Handy method compares the iRODS release version of the target server, and
+	 * will indicate that the iRODS version being connected to is at or above
+	 * the given version.
+	 * 
+	 * @param releaseVersion
+	 *            <code>String</code> in standard iRODS version format that will
+	 *            be checked against the currently-connected server.
+	 * @return <code>boolean</code> that will be <code>true</code> if the iRODS
+	 *         server is at or above the <code>releaseVersion</code>
+	 */
+	public boolean isTheIrodsServerAtLeastAtTheGivenReleaseVersion(
+			final String releaseVersion) {
+		if (releaseVersion == null || releaseVersion.length() == 0) {
+			throw new IllegalArgumentException("null or empty releaseVersion");
+		}
+
+		// The result is a negative integer if this String object
+		// lexicographically precedes the argument string.
+		int compValue = getRelVersion().compareToIgnoreCase(releaseVersion);
+		return compValue > -1;
+
+	}
+
+	@Override
+	public String toString() {
+		char ret = '\n';
+		String tabOver = "    ";
+		StringBuilder builder = new StringBuilder();
+		builder.append("IRODS server properties");
+		builder.append(ret);
+		builder.append(tabOver);
+		builder.append("icat enabled?:");
+		builder.append(icatEnabled.toString());
+		builder.append(ret);
+
+		builder.append(tabOver);
+		builder.append("Server boot time:");
+		builder.append(serverBootTime);
+		builder.append(ret);
+
+		builder.append(tabOver);
+		builder.append("Rel version:");
+		builder.append(relVersion);
+		builder.append(ret);
+
+		builder.append(tabOver);
+		builder.append("API version:");
+		builder.append(apiVersion);
+		builder.append(ret);
+
+		builder.append(tabOver);
+		builder.append("zone:");
+		builder.append(rodsZone);
+		builder.append(ret);
+
+		return builder.toString();
+
+	}
+
+}
---
+++ iRODS/jargon/src/test/org/irods/jargon/core/packinstr/ExecCmdTest.java
@@ -0,0 +1,175 @@
+package org.irods.jargon.core.packinstr;
+
+import junit.framework.Assert;
+
+import org.irods.jargon.core.exception.JargonException;
+import org.junit.Test;
+
+public class ExecCmdTest {
+
+	@Test
+	public void testInstance() throws Exception {
+		String cmd = "hello";
+		String args = "";
+
+		ExecCmd execCmd = ExecCmd.instanceWithCommand(cmd, args);
+		Assert.assertNotNull(
+				"basic check fails, null returned from PI initializer", execCmd);
+		Assert.assertEquals("api number not set",
+				ExecCmd.STANDARD_EXEC_ENCAPSULATE_DATA_IN_RESPONSE_API_NBR,
+				execCmd.getApiNumber());
+	}
+
+	@Test
+	public void testInstanceIndicatingStreaming() throws Exception {
+		String cmd = "hello";
+		String args = "";
+
+		ExecCmd execCmd = ExecCmd
+				.instanceWithCommandAllowingStreamingForLargeResults(cmd, args);
+		Assert.assertNotNull(
+				"basic check fails, null returned from PI initializer", execCmd);
+		Assert.assertEquals("api number not set",
+				ExecCmd.EXEC_AND_STREAM_RESPONSE_API_NBR,
+				execCmd.getApiNumber());
+	}
+
+	@Test(expected = JargonException.class)
+	public void testInstanceBlankCommand() throws Exception {
+		String cmd = "";
+		String args = "";
+
+		ExecCmd.instanceWithCommand(cmd, args);
+	}
+
+	@Test(expected = JargonException.class)
+	public void testInstanceNullCommand() throws Exception {
+		String cmd = null;
+		String args = "";
+
+		ExecCmd.instanceWithCommand(cmd, args);
+	}
+
+	@Test(expected = JargonException.class)
+	public void testInstanceNullArgs() throws Exception {
+		String cmd = "hello";
+		String args = null;
+
+		ExecCmd.instanceWithCommand(cmd, args);
+	}
+
+	@Test
+	public void testGetParsedTags() throws Exception {
+		String cmd = "hello";
+		String args = "";
+
+		ExecCmd execCmd = ExecCmd.instanceWithCommand(cmd, args);
+		String actualXML = execCmd.getParsedTags();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("<ExecCmd241_PI><cmd>hello</cmd>\n");
+		sb.append("<cmdArgv></cmdArgv>\n");
+		sb.append("<execAddr></execAddr>\n");
+		sb.append("<hintPath></hintPath>\n");
+		sb.append("<addPathToArgv>0</addPathToArgv>\n");
+		sb.append("<KeyValPair_PI><ssLen>0</ssLen>\n");
+		sb.append("</KeyValPair_PI>\n");
+		sb.append("</ExecCmd241_PI>\n");
+
+		String expectedXML = sb.toString();
+
+		Assert.assertEquals("did not get expected XML from PI", expectedXML,
+				actualXML);
+	}
+
+	@Test
+	public void testGetParsedTagsNewerAPI() throws Exception {
+		String cmd = "hello";
+		String args = "";
+
+		ExecCmd execCmd = ExecCmd.instanceWithCommandUsingAlignedAPI(cmd, args);
+		String actualXML = execCmd.getParsedTags();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("<ExecCmd_PI><cmd>hello</cmd>\n");
+		sb.append("<cmdArgv></cmdArgv>\n");
+		sb.append("<execAddr></execAddr>\n");
+		sb.append("<hintPath></hintPath>\n");
+		sb.append("<addPathToArgv>0</addPathToArgv>\n");
+		sb.append("<dummy>0</dummy>\n");
+		sb.append("<KeyValPair_PI><ssLen>0</ssLen>\n");
+		sb.append("</KeyValPair_PI>\n");
+		sb.append("</ExecCmd_PI>\n");
+
+		String expectedXML = sb.toString();
+
+		Assert.assertEquals("did not get expected XML from PI", expectedXML,
+				actualXML);
+	}
+
+	@Test
+	public void testGetParsedTagsWithStreaming() throws Exception {
+		String cmd = "hello";
+		String args = "";
+
+		ExecCmd execCmd = ExecCmd
+				.instanceWithCommandAllowingStreamingForLargeResults(cmd, args);
+		String actualXML = execCmd.getParsedTags();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("<ExecCmd_PI><cmd>hello</cmd>\n");
+		sb.append("<cmdArgv></cmdArgv>\n");
+		sb.append("<execAddr></execAddr>\n");
+		sb.append("<hintPath></hintPath>\n");
+		sb.append("<addPathToArgv>0</addPathToArgv>\n");
+		sb.append("<dummy>0</dummy>\n");
+		sb.append("<KeyValPair_PI><ssLen>1</ssLen>\n");
+		sb.append("<keyWord>streamStdout</keyWord>\n");
+		sb.append("<svalue></svalue>\n");
+		sb.append("</KeyValPair_PI>\n");
+		sb.append("</ExecCmd_PI>\n");
+
+		String expectedXML = sb.toString();
+
+		Assert.assertEquals("did not get expected XML from PI", expectedXML,
+				actualXML);
+
+		Assert.assertEquals("did not set streaming api nbr",
+				ExecCmd.EXEC_AND_STREAM_RESPONSE_API_NBR,
+				execCmd.getApiNumber());
+
+	}
+
+	@Test
+	public void testInstanceWithAllParms() throws Exception {
+		String cmd = "hello";
+		String args = "";
+		String host = "host";
+		String absPath = "/an/abs/path";
+
+		ExecCmd execCmd = ExecCmd.instanceWithHostAndArgumentsToPassParameters(
+				cmd, args, host, absPath);
+		Assert.assertNotNull(
+				"basic check fails, null returned from PI initializer", execCmd);
+		Assert.assertEquals("api number not set",
+				ExecCmd.STANDARD_EXEC_ENCAPSULATE_DATA_IN_RESPONSE_API_NBR,
+				execCmd.getApiNumber());
+	}
+
+	@Test
+	public void testInstanceWithAllParmsBlankHost() throws Exception {
+		String cmd = "cmd";
+		String args = "";
+		String host = "";
+		String absPath = "/an/abs/path";
+
+		ExecCmd execCmd = ExecCmd.instanceWithHostAndArgumentsToPassParameters(
+				cmd, args, host, absPath);
+		Assert.assertNotNull(
+				"basic check fails, null returned from PI initializer", execCmd);
+		Assert.assertEquals("api number not set",
+				ExecCmd.STANDARD_EXEC_ENCAPSULATE_DATA_IN_RESPONSE_API_NBR,
+				execCmd.getApiNumber());
+	}
+
+}
