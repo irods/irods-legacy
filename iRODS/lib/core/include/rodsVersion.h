434d41553aea6f5f75bac974109391497a8db651
Wayne-Schroeder <schroeder@diceresearch.org>
2011-02-09 20:26:37 +0000

    Update the version number/date for the 2.5 release.
    It may be easier for people working with the svn version if we start
    calling it 2.5, even tho the release won't happen for a couple weeks.
    
    
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@3783 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/core/include/rodsVersion.h
+++ iRODS/lib/core/include/rodsVersion.h
@@ -9,9 +9,9 @@
 #ifndef RODS_VERSION_H
 #define RODS_VERSION_H
 
-#define RODS_REL_VERSION	"rods2.4.1"
+#define RODS_REL_VERSION	"rods2.5"
 #define RODS_API_VERSION	"d"
-#define RODS_RELEASE_DATE       "Oct 2010"
+#define RODS_RELEASE_DATE       "Feb 2010"
 
 
 #endif	/* RODS_VERSION_H */
