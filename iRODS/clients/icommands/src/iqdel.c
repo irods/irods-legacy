1ad46df8b120959f9e4605f5e3a3655da41e3728
mywan <mwan@diceresearch.org>
2011-05-10 23:33:37 +0000

    add -u and -a options
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4002 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/icommands/src/iqdel.c
+++ iRODS/clients/icommands/src/iqdel.c
@@ -12,6 +12,7 @@
 #define BIG_STR 200
 
 void usage();
+int qdelUtil (rcComm_t *conn, char *userName, int allFlag, rodsEnv *myEnv);
 
 int debug=0;
 
@@ -47,7 +48,7 @@ main(int argc, char **argv) {
 
    rodsLogLevel(LOG_ERROR);  /* This should be the default someday */
 
-   status = parseCmdLineOpt (argc, argv, "vVh", 0, &myRodsArgs);
+   status = parseCmdLineOpt (argc, argv, "au:vVh", 0, &myRodsArgs);
    if (status) {
       printf("Use -h for help\n");
       exit(1);
@@ -81,8 +82,14 @@ main(int argc, char **argv) {
       if (!debug) exit (3);
    }
 
-   for (i=argOffset;i<argc;i++) {
-      status = rmDelayedRule(argv[i]);
+   if (myRodsArgs.all) {
+      status = qdelUtil (Conn, NULL, myRodsArgs.all, &myEnv);
+   } else if (myRodsArgs.user) {
+      status = qdelUtil (Conn, myRodsArgs.userString, myRodsArgs.all, &myEnv);
+   } else {
+      for (i=argOffset;i<argc;i++) {
+         status = rmDelayedRule(argv[i]);
+      }
    }
 
    rcDisconnect(Conn);
@@ -91,12 +98,70 @@ main(int argc, char **argv) {
    exit(0);
 }
 
+int 
+qdelUtil (rcComm_t *conn, char *userName, int allFlag, rodsEnv *myEnv)
+{
+    genQueryInp_t genQueryInp;
+    int status, savedStatus, i, continueInx;
+    char tmpStr[MAX_NAME_LEN];
+    sqlResult_t *execId;
+    char *execIdStr;
+    genQueryOut_t *genQueryOut = NULL;
+
+    if (allFlag == 1 && userName != NULL) {
+        rodsLog (LOG_ERROR, 
+          "qdelUtil: all(-a) and  user(-u) cannot be used together");
+        return USER_INPUT_OPTION_ERR;
+    }
+    memset (&genQueryInp, 0, sizeof (genQueryInp_t));
+    if (userName != NULL) {
+	snprintf (tmpStr, MAX_NAME_LEN, " = '%s'", userName);
+	addInxVal (&genQueryInp.sqlCondInp, COL_RULE_EXEC_USER_NAME, tmpStr);
+    }
+    addInxIval (&genQueryInp.selectInp, COL_RULE_EXEC_ID, 1);
+    genQueryInp.maxRows = MAX_SQL_ROWS;
+
+    continueInx = 1;    /* a fake one so it will do the first query */
+    while (continueInx > 0) {
+        status =  rcGenQuery (conn, &genQueryInp, &genQueryOut);
+        if (status < 0) {
+            if (status != CAT_NO_ROWS_FOUND) savedStatus = status;
+	    break;
+        }
+
+        if ((execId = getSqlResultByInx (genQueryOut, COL_RULE_EXEC_ID)) ==
+          NULL) {
+            rodsLog (LOG_ERROR,
+              "qdelUtil: getSqlResultByInx for COL_RULE_EXEC_ID failed");
+            return (UNMATCHED_KEY_OR_INDEX);
+        }
+        for (i = 0;i < genQueryOut->rowCnt; i++) {
+            execIdStr = &execId->value[execId->len * i];
+	    status = rmDelayedRule (execIdStr);
+	    if (status < 0) {
+                rodsLog (LOG_ERROR,
+                  "qdelUtil: rmDelayedRule %s error. status = %d", 
+                  execIdStr, status);
+	        savedStatus = status;
+	    }
+        }
+	continueInx = genQueryInp.continueInx = genQueryOut->continueInx;
+        freeGenQueryOut (&genQueryOut);
+    }
+    clearGenQueryInp (&genQueryInp);
+    return savedStatus;
+}
+
 void usage()
 {
    printf("Usage: iqdel [-vVh] ruleId [...]\n");
+   printf("Usage: iqdel [-a] [-u user]\n");
    printf("\n");
    printf(" iqdel removes delayed rules from the queue.\n");
    printf(" multiple ruleIds may be included on the command line.\n");
+   printf(" The -a option specifies the removal of all delayed rules\n");
+   printf(" The -u option specifies the removal of all delayed rules of the fiven user\n");
+   printf(" The -a and -u options cannot be used together\n");
    printf("\n");
    printf("Also see iqstat and iqmod.\n");
    printReleaseInfo("iqdel");
