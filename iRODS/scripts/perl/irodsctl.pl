024ad420fa1f8a31ddcfc9dff9df6cb7d95c9817
Arcot-Rajasekar <sekar@diceresearch.org>
2011-08-11 15:22:44 +0000

    added options for turning on the irods rule debugger
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4301 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/scripts/perl/irodsctl.pl
+++ iRODS/scripts/perl/irodsctl.pl
@@ -222,12 +222,17 @@ $postgresBinDir  = File::Spec->catdir( $POSTGRES_HOME, "bin" );
 # note that this can lead to slower performance
 # $PREPOSTPROCFORGENQUERYFLAG=1;
 
-# GLOBALREDEBUGFLAG - turn this on if you want debugging to be
-# enabled across the board.Make sure that the XmsgServer is also running. 
-# REMEMBER This may generate lots and lots of messages!!!! 
-# Please set the value to 3 only. It prints out rules and micro-services. 
-# $GLOBALREDEBUGFLAG=3;
+# GLOBALREAUDITFLAG - turn this on if you want rule and micro-service auditing
+# enabled across the board.Make sure that the XmsgServer is also running.
+# REMEMBER This may generate lots and lots of messages!!!!
+# Please set the value to 3 only. It prints out rules and micro-services.
+$GLOBALREAUDITFLAG=3;
 
+# GLOBALREDEBUGFLAG - turn this on if you want debugging to be
+# enabled across the board.Make sure that the XmsgServer is also running.
+# Please set the value to 4 only. You can interact with the debugger through
+# idbug
+$GLOBALREDEBUGFLAG=4;
 
 # $DefFileMode - the mode of the file created in the resource vault. 
 # The default value is 0600 (DEFAULT_FILE_MODE).
@@ -256,6 +261,7 @@ if ($irodsReconnect)		{ $ENV{'irodsReconnect'}    = $irodsReconnect; }
 if ($RETESTFLAG)		{ $ENV{'RETESTFLAG'}          = $RETESTFLAG; }
 if ($GLOBALALLRULEEXECFLAG)    { $ENV{'GLOBALALLRULEEXECFLAG'} = $GLOBALALLRULEEXECFLAG; }
 if ($PREPOSTPROCFORGENQUERYFLAG)    { $ENV{'PREPOSTPROCFORGENQUERYFLAG'} = $PREPOSTPROCFORGENQUERYFLAG; }
+if ($GLOBALREAUDITFLAG)         { $ENV{'GLOBALREAUDITFLAG'}   = $GLOBALREAUDITFLAG; }
 if ($GLOBALREDEBUGFLAG)         { $ENV{'GLOBALREDEBUGFLAG'}   = $GLOBALREDEBUGFLAG; }
 if ($DefFileMode)		{ $ENV{'DefFileMode'}         = $DefFileMode; }
 if ($DefDirMode)		{ $ENV{'DefDirMode'}          = $DefDirMode; }
