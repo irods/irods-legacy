397ccfe5fd3a9a19f028bc263885e69e4a2b6f66
xu-hao <xuh@email.unc.edu>
2011-09-02 06:08:51 +0000

    Update rule example to reflect recent changes
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4405 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/icommands/test/rules3.0/ruleworkflowapplyAllRules.r
+++ iRODS/clients/icommands/test/rules3.0/ruleworkflowapplyAllRules.r
@@ -1,12 +1,12 @@
 myTestRule {
 #Input parameters are:
-#  Name of action to perform
+#  Action to perform
 #  Flag for whether to save REI structure, 1 is yes
 #  Flag for whether to apply recursively, 1 is yes
 #Output from executing the example is:
 #  print 1
 #  print 2
-    applyAllRules("print", *SaveREI, *All);
+    applyAllRules(print, *SaveREI, *All);
 }
 print{
     or {
