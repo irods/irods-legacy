ac07699cb7b90aff89290b579b42c79148722c18
mywan <mwan@diceresearch.org>
2011-09-19 23:44:42 +0000

    Doxygen documentation. Not done yet for this API
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4432 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/api/src/rcDataObjCopy.c
+++ iRODS/lib/api/src/rcDataObjCopy.c
@@ -1,9 +1,79 @@
+/**
+ * @file  rcDataObjCopy.c
+ *
+ */
 /*** Copyright (c), The Regents of the University of California            ***
  *** For more information please refer to files in the COPYRIGHT directory ***/
 /* This is script-generated code.  */ 
 /* See dataObjCopy.h for a description of this API call.*/
 
 #include "dataObjCopy.h"
+
+/**
+ * \fn rcDataObjCopy (rcComm_t *conn, dataObjCopyInp_t *dataObjCopyInp)
+ *
+ * \brief Copy a data object from a iRODS path to another a iRODS path. 
+ *
+ * \user client
+ *
+ * \category data object operations
+ *
+ * \since 1.0
+ *
+ * \author  Mike Wan
+ * \date    2007
+ *
+ * \remark none
+ *
+ * \note none
+ *
+ * \usage
+ * Copy a data object /myZone/home/john/myfile in myRescource for write:
+ * \n dataObjInp_t dataObjInp;
+ * \n bzero (&dataObjInp, sizeof (dataObjInp));
+ * \n rstrcpy (dataObjInp.objPath, "/myZone/home/john/myfile", MAX_NAME_LEN);
+ * \n dataObjInp.openFlags = O_WRONLY;
+ * \n addKeyVal (&dataObjInp.condInput, RESC_NAME_KW, "myRescource");
+ * \n status = rcDataObjCopy (conn, &dataObjInp);
+ * \n if (status < 0) {
+ * \n .... handle the error
+ * \n }
+ *
+ * \param[in] conn - A rcComm_t connection handle to the server.
+ * \param[in] dataObjCopyInp_t - Elements of dataObjInp_t used :
+ *    \li char \b objPath[MAX_NAME_LEN] - full path of the data object.
+ *    \li int \b destDataObjInp.numThreads - the number of threads to use. 
+ *        Valid values are:
+ *      \n NO_THREADING (-1) - no multi-thread
+ *      \n 0 - the server will decide the number of threads.
+ *        (recommanded setting).
+ *      \n A positive integer - specifies the number of threads.
+ *    \li keyValPair_t \b destDataObjInp.condInput - keyword/value pair input. 
+ *       Valid keywords:
+ *    \n DATA_TYPE_KW - the data type of the data object.
+ *    \n FORCE_FLAG_KW - overwrite existing target. This keyWd has no value
+ *    \n REG_CHKSUM_KW -  register the target checksum value after the copy.
+ *	      This keyWd has no value.
+ *    \n VERIFY_CHKSUM_KW - verify and register the target checksum value 
+ *            after the copy. This keyWd has no value.
+ *    \n DEST_RESC_NAME_KW - The resource to store this data object
+ *    \n FILE_PATH_KW - The physical file path for this data object if the
+ *             normal resource vault is not used.
+ *    \n RBUDP_TRANSFER_KW - use RBUDP for data transfer. This keyWd has no 
+ *             value
+ *    \n RBUDP_SEND_RATE_KW - the number of RBUDP packet to send per second 
+ *	    The default is 600000
+ *    \n RBUDP_PACK_SIZE_KW - the size of RBUDP packet. The default is 8192 
+ *
+ * \return integer
+ * \retval an opened object descriptor on success
+
+ * \sideeffect none
+ * \pre none
+ * \post none
+ * \sa none
+ * \bug  no known bugs
+**/
 
 int
 rcDataObjCopy (rcComm_t *conn, dataObjCopyInp_t *dataObjCopyInp)
