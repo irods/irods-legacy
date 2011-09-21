9aba19d6c41ad984020e9396b5cb66fb39517178
Wayne-Schroeder <schroeder@diceresearch.org>
2011-09-21 20:54:09 +0000

    Test from srbbrick8
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4443 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/core/include/rodsVersion.h
+++ iRODS/lib/core/include/rodsVersion.h
@@ -9,7 +9,7 @@
 #ifndef RODS_VERSION_H
 #define RODS_VERSION_H
 
-#define RODS_REL_VERSION	"rods3.0beta"
+#define RODS_REL_VERSION	"rods3.0"
 #define RODS_API_VERSION	"d"
 #define RODS_RELEASE_DATE       "Sept 2011"
 
