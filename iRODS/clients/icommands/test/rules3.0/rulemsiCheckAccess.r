6f5a2545227fafa94c0b44e1a88d6266bc0272b3
xu-hao <xuh@email.unc.edu>
2011-09-30 02:01:36 +0000

    Fix a type error in a rule example
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4461 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/icommands/test/rules3.0/rulemsiCheckAccess.r
+++ iRODS/clients/icommands/test/rules3.0/rulemsiCheckAccess.r
@@ -5,7 +5,7 @@ myTestRule {
 #Output parameter is:
 #  Result, 0 for failure and 1 for success
   msiCheckAccess(*Path,*Acl,*Result);
-  if(*Result == "0") {
+  if(*Result == 0) {
     writeLine("stdout","File *Path does not have access *Acl"); }
   else {writeLine("stdout","File *Path has access *Acl"); }
 }
