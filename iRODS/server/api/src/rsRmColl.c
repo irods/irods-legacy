9aaefb9339d4b9d18638826c72d619743cb5bea9
mywan <mwan@diceresearch.org>
2011-11-08 00:40:47 +0000

    fix a warning msg
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4560 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/api/src/rsRmColl.c
+++ iRODS/server/api/src/rsRmColl.c
@@ -294,8 +294,8 @@ dataObjInfo_t *dataObjInfo, collOprStat_t **collOprStat)
     }
     rsCloseCollection (rsComm, &handleInx);
 
-    if (rmtrashFlag > 0 && (isTrashHome (rmCollInp->collName) > 0 || 
-      isOrphanPath (rmCollInp->collName) == is_ORPHAN_HOME) ||
+    if ((rmtrashFlag > 0 && isTrashHome (rmCollInp->collName) > 0) || 
+      isOrphanPath (rmCollInp->collName) == is_ORPHAN_HOME ||
       isBundlePath (rmCollInp->collName) == True) {
         /* don't rm user's home trash coll or orphan collection */
         status = 0;
