2a3b2effcbf1501b299065663735ba26ead0066d
mywan <mwan@diceresearch.org>
2011-12-01 23:30:25 +0000

    Handle O_TRUNC for osx_platform in rcFilePut
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4614 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/api/src/rcFilePut.c
+++ iRODS/lib/api/src/rcFilePut.c
@@ -10,6 +10,13 @@ rcFilePut (rcComm_t *conn, fileOpenInp_t *filePutInp,
 bytesBuf_t *filePutInpBBuf)
 {
     int status;
+
+#if defined(osx_platform)
+    if (filePutInp->flags & O_TRUNC) {
+        filePutInp->flags = filePutInp->flags ^ O_TRUNC;
+        filePutInp->flags = filePutInp->flags | 0x200;
+    }
+#endif
     status = procApiRequest (conn, FILE_PUT_AN, filePutInp, filePutInpBBuf, 
         (void **) NULL, NULL);
 
