b82dc824c51efc3ab9e1cd450a96a3f5b5540235
mywan <mwan@diceresearch.org>
2012-02-23 23:57:04 +0000

    Put the 7 Netcdf micro-service altogether
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4813 ea137bd6-aa86-47b6-b90d-b101d9f19a90

---
+++ iRODS/server/re/include/reNetcdf.h
@@ -0,0 +1,43 @@
+/*** Copyright (c), The Regents of the University of California            ***
+ *** For more information please refer to files in the COPYRIGHT directory ***/
+
+/* reSysDataObjOpr.h - header file for reSysDataObjOpr.c
+ */
+
+
+
+#ifndef RE_NETCDF_H
+#define RE_NETCDF_H
+
+#include "apiHeaderAll.h"
+#include "reGlobalsExtern.h"
+#include "rsGlobalExtern.h"
+
+int
+msiNcOpen (msParam_t *inpParam1, msParam_t *inpParam2, msParam_t *outParam,
+ruleExecInfo_t *rei);
+int
+msiNcCreate (msParam_t *inpParam1, msParam_t *inpParam2, msParam_t *outParam,
+ruleExecInfo_t *rei);
+int
+msiNcClose (msParam_t *inpParam1, ruleExecInfo_t *rei);
+int
+msiNcInqId (msParam_t *inpParam1, msParam_t *inpParam2, msParam_t *inpParam3,
+msParam_t *outParam, ruleExecInfo_t *rei);
+int
+msiNcInqWithId (msParam_t *inpParam1, msParam_t *inpParam2,
+msParam_t *inpParam3, msParam_t *outParam, ruleExecInfo_t *rei);
+int
+msiNcGetVarsByType (msParam_t *dataTypeParam, msParam_t *ncidParam,
+msParam_t *varidParam, msParam_t *ndimParam, msParam_t *startParam,
+msParam_t *countParam, msParam_t *strideParam,
+msParam_t *outParam, ruleExecInfo_t *rei);
+#ifdef LIB_CF
+int
+msiNccfGetVara (msParam_t *ncidParam, msParam_t *varidParam,
+msParam_t *lvlIndexParam, msParam_t *timestepParam,
+msParam_t *latRange0Param, msParam_t *latRange1Param,
+msParam_t *lonRange0Param, msParam_t *lonRange1Param,
+msParam_t *maxOutArrayLenParam, msParam_t *outParam, ruleExecInfo_t *rei);
+#endif
+#endif	/* RE_NETCDF_H */
--- iRODS/server/re/src/reNetcdf.c
+++ iRODS/server/re/src/reNetcdf.c
@@ -6,12 +6,8 @@
 /*** Copyright (c), The Regents of the University of California            ***
  *  *** For more information please refer to files in the COPYRIGHT directory ***/
 
-#if 0
-#include "reNetcdf.h"
-#endif
-#include "apiHeaderAll.h"
-#include "rsApiHandler.h"
 #include "collection.h"
+#include "reNetcdf.h"
 
 int
 msiNcOpen (msParam_t *inpParam1, msParam_t *inpParam2, msParam_t *outParam, 
@@ -65,6 +61,64 @@ ruleExecInfo_t *rei)
     } else {
       rodsLogAndErrorMsg (LOG_ERROR, &rsComm->rError, rei->status,
         "msiNcOpen: rsNcOpen failed for %s, status = %d",
+        ncOpenInp.objPath, rei->status);
+    }
+
+    return (rei->status);
+}
+
+int
+msiNcCreate (msParam_t *inpParam1, msParam_t *inpParam2, msParam_t *outParam, 
+ruleExecInfo_t *rei)
+{
+    rsComm_t *rsComm;
+    ncOpenInp_t ncOpenInp;
+    int *ncid;
+
+    RE_TEST_MACRO ("    Calling msiNcCreate")
+
+    if (rei == NULL || rei->rsComm == NULL) {
+      rodsLog (LOG_ERROR,
+        "msiNcCreate: input rei or rsComm is NULL");
+      return (SYS_INTERNAL_NULL_INPUT_ERR);
+    }
+
+    rsComm = rei->rsComm;
+    if (inpParam1 == NULL) {
+        rodsLog (LOG_ERROR,
+          "msiNcCreate: input inpParam1 is NULL");
+        return (SYS_INTERNAL_NULL_INPUT_ERR);
+    }
+
+    if (strcmp (inpParam1->type, STR_MS_T) == 0) {
+        /* str input */
+	bzero (&ncOpenInp, sizeof (ncOpenInp));
+	rstrcpy (ncOpenInp.objPath, (char*)inpParam1->inOutStruct, 
+	  MAX_NAME_LEN);
+    } else  if (strcmp (inpParam1->type, NcOpenInp_MS_T) == 0) {
+	ncOpenInp = *((ncOpenInp_t *) inpParam1->inOutStruct);
+	replKeyVal (&((ncOpenInp_t *) inpParam1->inOutStruct)->condInput,
+	  &ncOpenInp.condInput);
+    } else {
+        rodsLog (LOG_ERROR,
+          "msiNcOpen: Unsupported input Param1 type %s",
+          inpParam1->type);
+        return (USER_PARAM_TYPE_ERR);
+    }
+    if (inpParam2 != NULL) {
+	/* parse for mode */
+	ncOpenInp.mode = parseMspForPosInt (inpParam2);
+	if (ncOpenInp.mode < 0) return (ncOpenInp.mode);
+    }
+
+    rei->status = rsNcCreate (rsComm, &ncOpenInp, &ncid);
+    clearKeyVal (&ncOpenInp.condInput);
+    if (rei->status >= 0) {
+        fillIntInMsParam (outParam, *ncid);
+        free (ncid);
+    } else {
+      rodsLogAndErrorMsg (LOG_ERROR, &rsComm->rError, rei->status,
+        "msiNcCreate: rsNcCreate failed for %s, status = %d",
         ncOpenInp.objPath, rei->status);
     }
 
@@ -338,6 +392,7 @@ msParam_t *outParam, ruleExecInfo_t *rei)
     return (rei->status);
 }
 
+#ifdef LIB_CF
 int
 msiNccfGetVara (msParam_t *ncidParam, msParam_t *varidParam, 
 msParam_t *lvlIndexParam, msParam_t *timestepParam, 
@@ -419,5 +474,6 @@ msParam_t *maxOutArrayLenParam, msParam_t *outParam, ruleExecInfo_t *rei)
 
     return (rei->status);
 }
+#endif	
 
 
