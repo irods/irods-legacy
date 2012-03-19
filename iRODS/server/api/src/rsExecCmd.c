3ae9b30447956566e928619aed1caadd36a3280b
adetorcy <adetorcy@email.unc.edu>
2012-03-19 21:54:31 +0000

    Undid troublesome free() on Solaris
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4887 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/api/src/rsExecCmd.c
+++ iRODS/server/api/src/rsExecCmd.c
@@ -240,7 +240,7 @@ _rsExecCmd (rsComm_t *rsComm, execCmd_t *execCmdInp, execCmdOut_t **execCmdOut)
 	    char *myStr = (char*)malloc (NAME_LEN + 20);
 	    snprintf (myStr, NAME_LEN + 20, "%s=%s", EXEC_CMD_RULE_KW, tmpStr);
 	    putenv(myStr);
-		free(myStr);	// cppcheck - Memory leak: myStr
+//		free(myStr);	// don't free or environment is lost
 	}
 	close (stdoutFd[0]);
 	close (stderrFd[0]);
