3ba99b15d971b249f86a0b0fd8ee9ac31ebbd750
xu-hao <xuh@email.unc.edu>
2012-03-30 04:37:53 +0000

    Native support for gen query syntax in the rule language. A gen query expression evaluates to an object of type genQueryInp_t
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4904 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/re/src/nre.reLib1.c
+++ iRODS/server/re/src/nre.reLib1.c
@@ -1385,10 +1385,10 @@ insertRulesIntoDBNew(char * baseName, RuleSet *ruleSet,
 		  ruleNameToString(&p, &s, 0, ruleNode->subtrees[0]);
 		  p = ruleCondStr;
 		  s = MAX_RULE_LEN;
-		  termToString(&p, &s, 0, MIN_PREC, ruleNode->subtrees[1]);
+		  termToString(&p, &s, 0, MIN_PREC, ruleNode->subtrees[1], 0);
 		  p = ruleActionRecoveryStr;
 		  s = MAX_RULE_LEN;
-		  termToString(&p, &s, 0, MIN_PREC, ruleNode->subtrees[2]);
+		  termToString(&p, &s, 0, MIN_PREC, ruleNode->subtrees[2], 0);
 		  avu = lookupAVUFromMetadata(ruleNode->subtrees[4], "id");
 		  if(avu!=NULL) {
 			  rstrcpy(ruleIdStr, avu->subtrees[1]->text, MAX_NAME_LEN);
@@ -1403,7 +1403,7 @@ insertRulesIntoDBNew(char * baseName, RuleSet *ruleSet,
 		  ruleNameToString(&p, &s, 0, ruleNode->subtrees[0]);
 		  p = ruleCondStr;
 		  s = MAX_RULE_LEN;
-		  termToString(&p, &s, 0, MIN_PREC, ruleNode->subtrees[1]);
+		  termToString(&p, &s, 0, MIN_PREC, ruleNode->subtrees[1], 0);
 		  p = ruleActionRecoveryStr;
 		  s = MAX_RULE_LEN;
 		  actionsToString(&p, &s, 0, ruleNode->subtrees[2], ruleNode->subtrees[3]);
