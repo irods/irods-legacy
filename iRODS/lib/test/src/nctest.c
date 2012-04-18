60e3805f0e8daf297b27f947975c816120f15944
mywan <mwan@diceresearch.org>
2012-04-18 22:42:18 +0000

    Add test for the new rcNcInq API
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4933 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/test/src/nctest.c
+++ iRODS/lib/test/src/nctest.c
@@ -5,8 +5,8 @@
 #include "rodsClient.h" 
 
 /* a copy of sfc_pres_temp.nc can be found in ../netcdf/sfc_pres_temp.nc */
-#define TEST_PATH1 "/oneZone/home/rods/netcdf/sfc_pres_temp.nc"
-#define TEST_PATH2 "/oneZone/home/rods/netcdf/pres_temp_4D.nc"
+#define TEST_PATH1 "/wanZone/home/rods/netcdf/sfc_pres_temp.nc"
+#define TEST_PATH2 "/wanZone/home/rods/netcdf/pres_temp_4D.nc"
 
 int
 myInqVar (rcComm_t *conn, int ncid, char *name, int *dataType, int *ndim);
@@ -14,6 +14,10 @@ int
 nctest1 (rcComm_t *conn, char *ncpath);
 int
 nctest2 (rcComm_t *conn, char *ncpath);
+int
+nctestold (rcComm_t *conn, char *ncpath);
+int
+printVarOut (ncGenVarOut_t *var);
 
 int
 main(int argc, char **argv)
@@ -73,7 +77,7 @@ main(int argc, char **argv)
 }
 
 int
-nctest1 (rcComm_t *conn, char *ncpath)
+nctestold (rcComm_t *conn, char *ncpath)
 {
     int status, i;
     ncOpenInp_t ncOpenInp;
@@ -453,7 +457,7 @@ nctest1 (rcComm_t *conn, char *ncpath)
 } 
 
 int
-nctest2 (rcComm_t *conn, char *ncpath)
+nctest1 (rcComm_t *conn, char *ncpath)
 {
     int status;
     ncOpenInp_t ncOpenInp;
@@ -462,7 +466,7 @@ nctest2 (rcComm_t *conn, char *ncpath)
     ncInqInp_t ncInqInp;
     ncInqOut_t *ncInqOut = NULL;
 
-    printf ("----- nctest2 for %s ------\n\n\n", ncpath);
+    printf ("----- nctest1 for %s ------\n\n\n", ncpath);
 
     /* open an nc object */
     bzero (&ncOpenInp, sizeof (ncOpenInp_t));
@@ -551,3 +555,388 @@ myInqVar (rcComm_t *conn, int ncid, char *name, int *dataType, int *ndim)
     return tmpId;
 }
 
+int
+nctest2 (rcComm_t *conn, char *ncpath)
+{
+    int status, i;
+    ncOpenInp_t ncOpenInp;
+    ncCloseInp_t ncCloseInp;
+    ncInqInp_t ncInqInp;
+    ncInqOut_t *ncInqOut = NULL;
+    ncInqOut_t *timeDimNcInqOut, *levelDimNcInqOut, *lonDimNcInqOut, 
+      *latDimNcInqOut;
+    ncInqOut_t *lonVarNcInqOut, *latVarNcInqOut, *tempVarNcInqOut, 
+      *presVarNcInqOut;
+    int ncid;
+    ncGetVarInp_t ncGetVarInp;
+    ncGetVarOut_t *ncGetVarOut = NULL;
+    rodsLong_t start[NC_MAX_DIMS], stride[NC_MAX_DIMS], count[NC_MAX_DIMS];
+#ifdef LIB_CF
+    nccfGetVarInp_t nccfGetVarInp;
+    nccfGetVarOut_t *nccfGetVarOut = NULL;
+    float *mydata;
+#endif
+    printf ("----- nctest2 for %s ------\n\n\n", ncpath);
+
+    /* open an nc object */
+    bzero (&ncOpenInp, sizeof (ncOpenInp_t));
+    rstrcpy (ncOpenInp.objPath, ncpath, MAX_NAME_LEN);
+    ncOpenInp.mode = NC_NOWRITE;
+
+    status = rcNcOpen (conn, &ncOpenInp, &ncid);
+
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNcOpen error for %s", ncOpenInp.objPath);
+        return status;
+    }
+
+    /* inq the time dim length */
+
+    bzero (&ncInqInp, sizeof (ncInqInp));
+    ncInqInp.ncid = ncid;
+    ncInqInp.paramType = NC_DIM_TYPE;
+    rstrcpy (ncInqInp.name, "time", MAX_NAME_LEN);
+    ncInqInp.flags = 0;
+
+    status = rcNcInq (conn, &ncInqInp, &ncInqOut);
+    if (status < 0) {
+	timeDimNcInqOut = NULL;
+    } else {
+	timeDimNcInqOut = ncInqOut;
+        printf ("%s ---- dim length = %lld\n", ncInqInp.name,
+          timeDimNcInqOut->dim[0].arrayLen);
+
+    }
+
+   /* inq the level length */
+    rstrcpy (ncInqInp.name, "level", MAX_NAME_LEN);
+    ncInqInp.flags = 0;
+
+    status = rcNcInq (conn, &ncInqInp, &ncInqOut);
+    if (status < 0) {
+	levelDimNcInqOut = NULL;
+    } else {
+        levelDimNcInqOut = ncInqOut;
+        printf ("%s ---- dim length = %lld\n", ncInqInp.name,
+          levelDimNcInqOut->dim[0].arrayLen);
+    }
+
+   /* inq the longitude length */
+    rstrcpy (ncInqInp.name, "longitude", MAX_NAME_LEN);
+    ncInqInp.flags = 0;
+
+    status = rcNcInq (conn, &ncInqInp, &ncInqOut);
+    if (status < 0) {
+	lonDimNcInqOut = NULL;
+    } else {
+        lonDimNcInqOut = ncInqOut;
+        printf ("%s ---- dim length = %lld\n", ncInqInp.name,
+          lonDimNcInqOut->dim[0].arrayLen);
+    }
+
+   /* inq the latitude length */
+    rstrcpy (ncInqInp.name, "latitude", MAX_NAME_LEN);
+    ncInqInp.flags = 0;
+
+    status = rcNcInq (conn, &ncInqInp, &ncInqOut);
+    if (status < 0) {
+	latDimNcInqOut = NULL;
+    } else {
+        latDimNcInqOut = ncInqOut;
+        printf ("%s ---- dim length = %lld\n", ncInqInp.name,
+          latDimNcInqOut->dim[0].arrayLen);
+    }
+
+    /* inq the longitude var */
+
+    bzero (&ncInqInp, sizeof (ncInqInp));
+    ncInqInp.ncid = ncid;
+    ncInqInp.paramType = NC_VAR_TYPE;
+    rstrcpy (ncInqInp.name, "longitude", MAX_NAME_LEN);
+    ncInqInp.flags = 0;
+
+    status = rcNcInq (conn, &ncInqInp, &ncInqOut);
+    if (status < 0) {
+        lonVarNcInqOut = NULL;
+    } else {
+        lonVarNcInqOut = ncInqOut;
+	printVarOut (lonVarNcInqOut->var);
+    }
+
+   /* inq the latitude var */
+    rstrcpy (ncInqInp.name, "latitude", MAX_NAME_LEN);
+    ncInqInp.flags = 0;
+
+    status = rcNcInq (conn, &ncInqInp, &ncInqOut);
+    if (status < 0) {
+        latVarNcInqOut = NULL;
+    } else {
+        latVarNcInqOut = ncInqOut;
+	printVarOut (latVarNcInqOut->var);
+    }
+
+   /* inq the temperature var */
+    rstrcpy (ncInqInp.name, "temperature", MAX_NAME_LEN);
+    ncInqInp.flags = 0;
+
+    status = rcNcInq (conn, &ncInqInp, &ncInqOut);
+    if (status < 0) {
+        tempVarNcInqOut = NULL;
+    } else {
+        tempVarNcInqOut = ncInqOut;
+        printVarOut (tempVarNcInqOut->var);
+    }
+
+   /* inq the pressure var */
+    rstrcpy (ncInqInp.name, "pressure", MAX_NAME_LEN);
+    ncInqInp.flags = 0;
+
+    status = rcNcInq (conn, &ncInqInp, &ncInqOut);
+    if (status < 0) {
+        presVarNcInqOut = NULL;
+    } else {
+        presVarNcInqOut = ncInqOut;
+        printVarOut (presVarNcInqOut->var);
+    }
+
+    /* get the variable values */
+    start[0] = 0;
+    count[0] = lonDimNcInqOut->dim[0].arrayLen;
+    stride[0] = 1;
+    bzero (&ncGetVarInp, sizeof (ncGetVarInp));
+    ncGetVarInp.dataType = lonVarNcInqOut->var->dataType;
+    ncGetVarInp.ncid = ncid;
+    ncGetVarInp.varid = lonVarNcInqOut->var->id;
+    ncGetVarInp.ndim = lonVarNcInqOut->var->nvdims;
+    ncGetVarInp.start = start;
+    ncGetVarInp.count = count;
+    ncGetVarInp.stride = stride;
+
+    status = rcNcGetVarsByType (conn, &ncGetVarInp, &ncGetVarOut);
+
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNcGetVarsByType error for longitude of %s", 
+	  lonVarNcInqOut->var->name, ncOpenInp.objPath);
+        return status;
+    } else {
+        printf ("longitude value: \n");
+        for (i = 0; i < ncGetVarOut->dataArray->len; i++) {
+            float *fdata = (float *)  ncGetVarOut->dataArray->buf;
+            printf ("  %.2f", fdata[i]);
+        }
+        printf ("\n");
+        freeNcGetVarOut (&ncGetVarOut);
+    }
+
+    /* variable latitude */
+    start[0] = 0;
+    count[0] = latDimNcInqOut->dim[0].arrayLen;
+    stride[0] = 1;
+    ncGetVarInp.dataType = latVarNcInqOut->var->dataType;
+    ncGetVarInp.varid = latVarNcInqOut->var->id;
+    ncGetVarInp.ndim = latVarNcInqOut->var->nvdims;
+
+    status = rcNcGetVarsByType (conn, &ncGetVarInp, &ncGetVarOut);
+
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNcGetVarsByType error for latitude of %s", 
+	  latVarNcInqOut->var->name, ncOpenInp.objPath);
+        return status;
+    } else {
+        printf ("latitude value: \n");
+        for (i = 0; i < ncGetVarOut->dataArray->len; i++) {
+            float *fdata = (float *)  ncGetVarOut->dataArray->buf;
+            printf ("  %.2f", fdata[i]);
+        }
+        printf ("\n");
+        freeNcGetVarOut (&ncGetVarOut);
+    }
+
+    if (timeDimNcInqOut != NULL) {
+        start[0] = 0;
+        start[1] = 0;
+        start[2] = 0;
+        start[3] = 0;
+        count[0] = timeDimNcInqOut->dim->arrayLen;
+        count[1] = levelDimNcInqOut->dim->arrayLen;
+        count[2] = latDimNcInqOut->dim->arrayLen;
+        count[3] = lonDimNcInqOut->dim->arrayLen;
+        stride[0] = 1;
+        stride[1] = 1;
+        stride[2] = 1;
+        stride[3] = 1;
+    } else {
+        start[0] = 0;
+        start[1] = 0;
+        count[0] = latDimNcInqOut->dim->arrayLen;
+        count[1] = lonDimNcInqOut->dim->arrayLen;
+        stride[0] = 1;
+        stride[1] = 1;
+    }
+    ncGetVarInp.dataType = presVarNcInqOut->var->dataType;
+    ncGetVarInp.varid = presVarNcInqOut->var->id;
+    ncGetVarInp.ndim = presVarNcInqOut->var->nvdims;
+
+    status = rcNcGetVarsByType (conn, &ncGetVarInp, &ncGetVarOut);
+
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNcGetVarsByType error for pressure of %s", 
+	  presVarNcInqOut->var->name, ncOpenInp.objPath);
+        return status;
+    } else {
+        printf ("pressure value: \n");
+        for (i = 0; i < ncGetVarOut->dataArray->len; i++) {
+            float *fdata = (float *)  ncGetVarOut->dataArray->buf;
+            printf ("  %.2f", fdata[i]);
+        }
+        printf ("\n");
+        freeNcGetVarOut (&ncGetVarOut);
+    }
+
+    /* temperature variable */
+    ncGetVarInp.dataType = tempVarNcInqOut->var->dataType;
+    ncGetVarInp.varid = tempVarNcInqOut->var->id;
+    ncGetVarInp.ndim = tempVarNcInqOut->var->nvdims;
+
+    status = rcNcGetVarsByType (conn, &ncGetVarInp, &ncGetVarOut);
+
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNcGetVarsByType error for temperature of %s", 
+	  tempVarNcInqOut->var->name, ncOpenInp.objPath);
+        return status;
+    } else {
+        printf ("temperature value: \n");
+        for (i = 0; i < ncGetVarOut->dataArray->len; i++) {
+            float *fdata = (float *)  ncGetVarOut->dataArray->buf;
+            printf ("  %.2f", fdata[i]);
+        }
+        printf ("\n");
+        freeNcGetVarOut (&ncGetVarOut);
+    }
+
+    freeNcInqOut (&timeDimNcInqOut);
+    freeNcInqOut (&levelDimNcInqOut);
+    freeNcInqOut (&lonDimNcInqOut);
+    freeNcInqOut (&latDimNcInqOut);
+    freeNcInqOut (&lonVarNcInqOut);
+    freeNcInqOut (&latVarNcInqOut);
+
+#ifdef LIB_CF
+    /* open an nc object */
+    bzero (&ncOpenInp, sizeof (ncOpenInp_t));
+    rstrcpy (ncOpenInp.objPath, TEST_PATH1, MAX_NAME_LEN);
+    ncOpenInp.mode = NC_NOWRITE;
+
+    status = rcNcOpen (conn, &ncOpenInp, &ncid);
+
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNcOpen error for %s", ncOpenInp.objPath);
+        return status;
+    }
+
+#if 0
+    tempvarid1 = myInqVar (conn, ncid1, "temperature", &temptype1, &tempndim);
+    if (tempvarid1 < 0)  return status;
+
+    presvarid1 = myInqVar (conn, ncid1, "pressure", &prestype1, &presndim);
+    if (presvarid1 < 0)  return status;
+#endif
+
+    /* pressure subset */
+    bzero (&nccfGetVarInp, sizeof (nccfGetVarInp));
+    nccfGetVarInp.ncid = ncid;
+    nccfGetVarInp.varid = presVarNcInqOut->var->id;
+    nccfGetVarInp.latRange[0] = 30.0;
+    nccfGetVarInp.latRange[1] = 41.0;
+    nccfGetVarInp.lonRange[0] = -120.0;
+    nccfGetVarInp.lonRange[1] = -96.0;
+    nccfGetVarInp.maxOutArrayLen = 1000;
+
+    status = rcNccfGetVara (conn, &nccfGetVarInp, &nccfGetVarOut);
+
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNccfGetVara error for %s", ncOpenInp.objPath);
+        return status;
+    }
+
+    printf (
+      "pressure subset: nlat = %d, nlon = %d, dataType = %d, arrayLen = %d\n",
+      nccfGetVarOut->nlat, nccfGetVarOut->nlon,
+      nccfGetVarOut->dataArray->type, nccfGetVarOut->dataArray->len);
+    mydata = (float *) nccfGetVarOut->dataArray->buf;
+    printf ("pressure values: ");
+    for (i = 0; i <  nccfGetVarOut->dataArray->len; i++) {
+        printf (" %f", mydata[i]);
+    }
+    printf ("\n");
+    freeNccfGetVarOut (&nccfGetVarOut);
+
+    /* temperature subset */
+    nccfGetVarInp.varid = tempVarNcInqOut->var->id;
+
+    status = rcNccfGetVara (conn, &nccfGetVarInp, &nccfGetVarOut);
+
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNccfGetVara error for %s", ncOpenInp.objPath);
+        return status;
+    }
+
+    printf (
+      "temperature subset: nlat = %d, nlon = %d, dataType = %d, arrLen = %d\n",
+      nccfGetVarOut->nlat, nccfGetVarOut->nlon,
+      nccfGetVarOut->dataArray->type, nccfGetVarOut->dataArray->len);
+    mydata = (float *) nccfGetVarOut->dataArray->buf;
+    printf ("temperature values: ");
+    for (i = 0; i <  nccfGetVarOut->dataArray->len; i++) {
+        printf (" %f", mydata[i]);
+    }
+    printf ("\n");
+    freeNccfGetVarOut (&nccfGetVarOut);
+#endif
+
+    /* close the file */
+    bzero (&ncCloseInp, sizeof (ncCloseInp_t));
+    ncCloseInp.ncid = ncid;
+    status = rcNcClose (conn, &ncCloseInp);
+    if (status < 0) {
+        rodsLogError (LOG_ERROR, status,
+          "rcNcClose error for %s", ncOpenInp.objPath);
+        return status;
+    }
+    freeNcInqOut (&tempVarNcInqOut);
+    freeNcInqOut (&presVarNcInqOut);
+
+    return 0;
+}
+
+int
+printVarOut (ncGenVarOut_t *var)
+{
+    int i;
+
+    printf ("%s ---- type = %d natts = %d, ndim = %d\n", var->name,
+      var->dataType, var->natts, var->nvdims);
+    printf ("   dimid:");
+    for (i = 0; i < var->nvdims; i++) {
+	printf ("    %d",  var->dimId[i]);
+    }
+    printf ("\n");
+    printf ("   attribute:\n");
+    for (i = 0; i < var->natts; i++) {
+	char tempStr[NAME_LEN];
+	void *bufPtr = (char *) var->att[i].value.dataArray->buf;
+        ncValueToStr (var->att[i].dataType, &bufPtr, tempStr);
+        printf ("    %s = %s\n",  var->att[i].name, tempStr);
+    }
+    printf ("\n");
+
+    return 0;
+}
