d814cc65d0cc8ff2cac1b12f26fa9137e9c95c4c
mywan <mwan@diceresearch.org>
2012-05-03 01:23:11 +0000

    add msiNccfGetVara test
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4950 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/icommands/test/rules3.0/netcdfTest1.r
+++ iRODS/clients/icommands/test/rules3.0/netcdfTest1.r
@@ -143,6 +143,49 @@ netcdfTest () {
 	    msiFreeNcStruct (*countArray);
 	    msiFreeNcStruct (*strideArray);
 	}
+# msiNccfGetVara test
+
+#        if (msiNcGetVarIdInInqOut (*ncInqOut, "temperature", *tempvarid) != 0) {
+#            writeLine("stdout", "msiNcGetVarIdInInqOut failed");
+#            fail;
+#        }
+# Need to do msiNcInqId for msiNccfGetVara to succeed
+       if (msiNcInqId ("temperature", 0, *ncid, *tempvarid) == 0) {
+            writeLine("stdout", "msiNcInqId success, tempvarid = *tempvarid");
+        } else {
+            writeLine("stdout", "msiNcInqId failed");
+            fail;
+        }
+
+        if (msiNccfGetVara (*ncid, *tempvarid, "0", "0", "30.0", "41.0", "-120.0", "-96.0", 1000, *tempVaraOut) == 0) {
+# inqOut is a struct.
+            writeLine("stdout", "msiNccfGetVara tempvarid success");
+            if (msiNcGetArrayLen (*tempVaraOut, *tempArrayLen) == 0) {
+                writeLine ("stdout", "tempArrayLen = *tempArrayLen");
+                if (msiNcGetDataType (*tempVaraOut, *tempDataType) == 0) {
+                    writeLine("stdout", "tempDataType = *tempDataType");
+                } else {
+                    writeLine("stdout", "msiNcGetDataType temp failed");
+                    fail;
+               }
+               for(*I=0;*I<*tempArrayLen;*I=*I+1) {
+                    msiNcGetElementInArray (*tempVaraOut, *I, *element);
+                    if (*tempDataType == 5) {
+# float. writeLine cannot handle float yet.
+                        msiFloatToString (*element, *floatStr);
+                        writeLine("stdout", "pressure *I: *floatStr");
+                    } else {
+                        writeLine("stdout", "pressure *I: *element");
+                    }
+                }
+            } else {
+                writeLine("stdout", "msiNcGetArrayLen failed");
+                fail;
+            }
+        } else {
+            writeLine("stdout", "msiNccfGetVara pressvarid failed");
+            fail;
+        }
         if (msiNcClose (*ncid) == 0) {
             writeLine("stdout", "msiNcClose success, ncid = *ncid");
         } else {
