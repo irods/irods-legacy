d2994c0de513de904a1bcc0ff364f50e1b4f334d
adetorcy <adetorcy@email.unc.edu>
2012-05-07 15:53:43 +0000

    improved version of rule example
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@4958 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/icommands/test/rules3.0/R_exec.r
+++ iRODS/clients/icommands/test/rules3.0/R_exec.r
@@ -2,28 +2,12 @@
 ###		R_exec.r: Runs an R program on a designated node (resource)
 ###		Assumes a link to R in <iRODS_DIR>/server/bin/cmd/ on that resource
 ###		AdT, 04/30/12
-###############################################################################################
+###############################################################################################  
+
 R_exec 
 {
-	### Split collection and object name, for queries
-	msiSplitPath(*inputObj, *collection, *object);
-	
-
-	### 1st query to see if obj exists on R-enabled resc
-	# Add select fields
-	msiAddSelectFieldToGenQuery("DATA_ID", "COUNT", *genQInp0);
-	# Add conditions
-	msiAddConditionToGenQuery("DATA_NAME", "=", *object, *genQInp0);
-	msiAddConditionToGenQuery("COLL_NAME", "=", *collection, *genQInp0);
-	msiAddConditionToGenQuery("DATA_RESC_NAME", "=", *rescName, *genQInp0);
-	# Run query
-	msiExecGenQuery(*genQInp0, *genQOut0);
-	# Extract file path and resource address
-  	foreach (*genQOut0)
-	{
-		msiGetValByKey(*genQOut0, "DATA_ID", *dataIdCount);
-	}
-	
+	### Check if object exists on R-enabled resource
+	isObjOnResc(*dataIdCount);
 
 	### Replicate obj to target resc if needed
 	if (int(*dataIdCount) == 0)
@@ -32,8 +16,71 @@ R_exec
 		msiDataObjRepl(*inputObj, *rescName, *foo);
 	}
 	
+	### Get file path and resource address
+	getFileInfo(*rescAddr, *filePath);
 	
-	### Get resource address and file path
+	### Invoke R on resource
+	# Watch for pathnames with spaces
+	*args="-f '*filePath'";
+	msiExecCmd("R", *args, *rescAddr, "null", "null", *cmdOut);
+	# Get stdout
+	msiGetStdoutInExecCmdOut(*cmdOut, *stdoutStr);
+	
+	### Save result in new object
+	# Create path with timestamp
+	msiGetSystemTime(*Time, "human");
+	*outputObj = *inputObj++".OUT."++*Time++".txt";
+	# Write to new object
+	msiStrlen(*stdoutStr, *len);
+	msiDataObjCreate(*outputObj, "", *W_FD);
+	msiDataObjWrite(*W_FD, *stdoutStr, *len);
+	msiDataObjClose(*W_FD, *foo1);
+	
+	### Extract metadata from R obj and adds it to result obj
+	extractMetadataFromObj(*outputObj);
+	
+	### Trim newly created replica
+	if (int(*dataIdCount) == 0)
+	then
+	{
+		msiDataObjTrim(*inputObj, *rescName, "null", "1", "null", *foo2);
+	}
+	
+	#Print stuff out if needed
+	#writeLine("stdout", *stdoutStr);
+}
+
+
+isObjOnResc(*dataIdCount)
+{
+	### Split collection and object name
+	msiSplitPath(*inputObj, *collection, *object);
+	
+	### Set up query
+	# Add select fields
+	msiAddSelectFieldToGenQuery("DATA_ID", "COUNT", *genQInp0);
+	# Add conditions
+	msiAddConditionToGenQuery("DATA_NAME", "=", *object, *genQInp0);
+	msiAddConditionToGenQuery("COLL_NAME", "=", *collection, *genQInp0);
+	msiAddConditionToGenQuery("DATA_RESC_NAME", "=", *rescName, *genQInp0);
+	
+	### Run query
+	msiExecGenQuery(*genQInp0, *genQOut0);
+	
+	### Extract data ID count
+  	foreach (*genQOut0)
+	{
+		msiGetValByKey(*genQOut0, "DATA_ID", *dataIdCount);
+	}
+}
+
+
+getFileInfo(*rescAddr, *filePath)
+{
+	### Split collection and object name
+	msiSplitPath(*inputObj, *collection, *object);
+
+	### Set up query
 	# Add select fields
 	msiAddSelectFieldToGenQuery("DATA_PATH", "null", *genQInp);
 	msiAddSelectFieldToGenQuery("RESC_LOC", "null", *genQInp);
@@ -49,38 +96,37 @@ R_exec
 		msiGetValByKey(*genQOut, "DATA_PATH", *filePath);
 		msiGetValByKey(*genQOut, "RESC_LOC", *rescAddr);
 	}
+}
+
+
+extractMetadataFromObj(*destObj)
+{
+	# Set read buffer length
+	*r_len=524288;	# 512KB
 	
+	# Read input object
+	*openArgs="objPath="++*inputObj++"++++rescName="++*rescName
+	msiDataObjOpen(*openArgs, *FD1);
+	msiDataObjRead(*FD1, *r_len, *r_buf);
+	msiDataObjClose(*FD1, *status);
 	
-	### Invoke R on resource
-	# Watch for pathnames with spaces
-	*args="-f '*filePath'";
-	msiExecCmd("R", *args, *rescAddr, "null", "null", *cmdOut);
-	# Get stdout
-	msiGetStdoutInExecCmdOut(*cmdOut, *stdoutStr);
+	# Read object that contains metadata extraction tags
+	msiDataObjOpen(*tagObj, *FD2);
+	msiDataObjRead(*FD2, *r_len, *r_buf2);
+	msiReadMDTemplateIntoTagStruct(*r_buf2, *tags);
+	msiDataObjClose(*FD2, *status);
 	
+	# Extract metadata from input object using tags
+	msiExtractTemplateMDFromBuf(*r_buf, *tags, *KVP);
 	
-	### Save result in new object
-	# Create path with timestamp
-	msiGetSystemTime(*Time, "human");
-	*outputObj = *inputObj++".OUT."++*Time++".txt";
-	# Write to new object
-	msiStrlen(*stdoutStr, *len);
-	msiDataObjCreate(*outputObj, "", *W_FD);
-	msiDataObjWrite(*W_FD, *stdoutStr, *len);
-	msiDataObjClose(*W_FD, *foo1);
+	# Add metadata to the object
+	msiGetObjType(*destObj, *objType);
+	msiAssociateKeyValuePairsToObj(*KVP, *destObj, *objType);
 	
-	
-	### Trim newly created replica
-	if (int(*dataIdCount) == 0)
-	then
-	{
-		msiDataObjTrim(*inputObj, *rescName, "null", "1", "null", *foo2);
-	}
-	
-	
-	#Print stuff out if needed
-	#writeLine("stdout", *stdoutStr);
-	
+	# Write out extracted metadata
+	writeKeyValPairs("stdout", *KVP, " : ");
 }  
-INPUT *inputObj="/renci/home/antoine/R/Negative Binomial test 1.R", *rescName="renci-vault1"
+
+
+INPUT *inputObj="/renci/home/antoine/R/Negative Binomial test 1 (with MD).R", *rescName="renci-vault1", *tagObj="/renci/home/antoine/R/R_meta.tag"
 OUTPUT ruleExecOut
