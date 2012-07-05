8b8570ed1d363e6df2418fcd823a9b12af322ff5
mywan <mwan@diceresearch.org>
2012-07-05 22:46:56 +0000

    Minor comment change
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@5057 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/test/src/pydapdir.c
+++ iRODS/lib/test/src/pydapdir.c
@@ -1,6 +1,6 @@
 /*** Copyright (c), The Regents of the University of California            ***
  *** For more information please refer to files in the COPYRIGHT directory ***/
-/* packtest.c - test the basic packing routines */
+/* pydapdir.c - test the basic routine for parsing a pydap web page */
 
 #include "rodsClient.h" 
 #include "regUtil.h" 
