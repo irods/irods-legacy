c7909aa4bc1b41a1d49a826c25056093d8d97dde
mywan <mwan@diceresearch.org>
2012-08-07 23:39:20 +0000

    Change default URL
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@5117 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/test/src/tdsdir.c
+++ iRODS/lib/test/src/tdsdir.c
@@ -12,11 +12,11 @@
 #if 0
 #define TDS_TOPDIR_FILE "tds/tdsData.xml"
 #define TDS_URL "http://hfrnet.ucsd.edu:8080/thredds/HFRADAR_USWC_hourly_RTV.xml"
-#define TDS_TOPDIR_FILE "tds/tdsDir.xml"
-#define TDS_URL         "http://hfrnet.ucsd.edu:8080/thredds/catalog.xml"
-#else
 #define TDS_TOPDIR_FILE "tds/tdsTopDir.xml"
 #define TDS_URL "http://motherlode.ucar.edu:8080/thredds/topcatalog.xml"
+#else
+#define TDS_TOPDIR_FILE "tds/tdsDir.xml"
+#define TDS_URL         "http://hfrnet.ucsd.edu:8080/thredds/catalog.xml"
 #endif
 #define THREDDS_DIR     "/thredds/"
 
