e4463dfc46b3cdf383f470d8fd3c83c7d131c1be
mywan <mwan@diceresearch.org>
2012-09-18 23:46:06 +0000

    Data Struct for NETCDF files aggregation
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@5194 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/api/include/ncOpen.h
+++ iRODS/lib/api/include/ncOpen.h
@@ -35,7 +35,7 @@ typedef struct {
 typedef struct {
     int startTime;
     int endTime;
-    int len;
+    int arraylen;
     int flags;	/* not used */
     char fileName[MAX_NAME_LEN];
 } ncAggElement_t;
@@ -44,9 +44,13 @@ typedef struct {
     int numFiles;
     int flags;		/* not used */
     char ncObjectName[MAX_NAME_LEN];
+    ncAggElement_t *ncAggElement;	/* pointer to numFiles of 
+                                         * ncAggElement_t */
 } ncAggregate_t;
     
-    
+#define NcAggElement_PI "int startTime; int endTime; int arraylen; int flags; str fileName[MAX_NAME_LEN];"
+#define NcAggregate_PI "int numFiles; int flags; str  ncObjectName[MAX_NAME_LEN]; struct *NcAggElement_PI(numFiles);"
+
 #if defined(RODS_SERVER) && defined(NETCDF_API)
 #define RS_NC_OPEN rsNcOpen
 /* prototype for the server handler */
