06d731d95aaa7bc0103aecb2f538038d884931a8
xu-hao <xuh@email.unc.edu>
2012-12-20 18:18:43 +0000

    fix a typing error in the previous bug fix
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@5359 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/api/src/rsGeneralAdmin.c
+++ iRODS/server/api/src/rsGeneralAdmin.c
@@ -304,9 +304,9 @@ _rsGeneralAdmin(rsComm_t *rsComm, generalAdminInp_t *generalAdminInp )
        if (strcmp(generalAdminInp->arg1,"group")==0) {
     	   userInfo_t ui;
     	   memset(&ui, 0, sizeof(userInfo_t));
-		   rei2.uoio = &ui;
-		   rstrcpy(ui.userName, generalAdminInp->arg4, NAME_LEN);
-		   rstrcpy(ui.rodsZone, generalAdminInp->arg4, NAME_LEN);
+    	   rei2.uoio = &ui;
+    	   rstrcpy(ui.userName, generalAdminInp->arg4, NAME_LEN);
+    	   rstrcpy(ui.rodsZone, generalAdminInp->arg5, NAME_LEN);
          /** RAJA ADDED June 1 2009 for pre-post processing rule hooks **/
          args[0] = generalAdminInp->arg2; /* groupname */
          args[1] = generalAdminInp->arg3; /* option */
