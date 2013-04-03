23210e58eca4bc3e01852e1be06a2a22860fd77c
Wayne-Schroeder <schroeder@diceresearch.org>
2013-04-03 17:48:25 +0000

    Remove boost compressed source file and build script, make an additional update to a test script to remove a now unneeded setting.
    This is a continuation of r5417 making changes to use system installed
    boost instead of the included version of boost (boost_irods.tar.bz2)
    which was no longer compatible.
    
    
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@5419 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/test/tbox/boostTest.sh
+++ iRODS/server/test/tbox/boostTest.sh
@@ -17,7 +17,7 @@ mv config.mk config.mk.old
 cat config.mk.old | sed 's/# USE_BOOST=1/USE_BOOST=1/g' > config.mk
 cd ..
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tbox/IRODS_BUILD/iRODS/boost_irods/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tbox/IRODS_BUILD/iRODS/boost_irods/lib
 
 make
 date
