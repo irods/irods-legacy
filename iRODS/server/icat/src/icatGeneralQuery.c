a8197118f80fd969139a92a52a86e58f58772086
Wayne-Schroeder <schroeder@diceresearch.org>
2013-06-05 18:23:48 +0000

    New version of some SQL used with quota enforcement that performs much better at scale.
    This new SQL and supporting mods in the ICAT function
    (generateSpecialQuery) were provided by Giacomo Mariani and colleagues
    at SuperComputing Applications and Innovation Department CINECA,
    Bologna, Italy, after investigating SQL that was running very slowly.
    
    I also checked the new SQL by hand to convince myself that the old and
    new SQL statements are logically equivalent, although both of them are
    a bit complicated.  It helps to work with the statements with a text
    editor, breaking them up into components (for the new, by the UNIONs).
    The new SQL is doing a UNION of 3 selects, the first of which is
    handling group quotas on specific-resources, the second is group
    quotas on total use, and the third is for both specific-resources and
    total-usage for the particular user.  It seems to be logically
    equivalent to the old version where the old had two OR conditions in
    some nested conditions (the new form there's one OR, and it's in the
    last select of the three).  One minor change from that provided is
    removing a trailing ';' on the SQL; this is not an issue with Postgres
    but may be with others.
    
    The two queries in this ICAT function are used when quotas are being
    enforced (i.e. by enabling quotas via the acRescQuotaPolicy rule in
    the core.re file).  There's a setRescQuota function that is called
    from the rsDataObjCreate API function, that calls chlGenQuery with
    this special option (QUOTA_QUERY).
    
    There are two SQL forms, but it's the second one, quotaQuery2, that is
    used in practice (as called by the rsDataObjCreate API function).
    
    I also made some minor changes to logging in the function, changing
    some rodsLog calls that were LOG_ERROR to LOG_DEBUG and I added some
    (also only at LOG_DEBUG (i.e. normally not logged)).
    
    As part of this commit, I've also updated the quotaTest.pl script to
    test this SQL.  See the instructions at the start of the script.  One
    needs to enable the enforcement via core.re first, and then run
    'quotaTest.pl enabled'.  But this test confirms that that SQL runs OK
    and that that quotas are enforced in the various modes (for users or
    groups, on particular resources or for total storage).  It's still
    worthwhile to run 'quotaTest.pl' without the 'enabled' option (with it
    off in core.re) to perform other tests.
    
    Thanks to Giacomo Mariani and colleagues for this improvement.
    
    
    
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@5465 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/server/icat/src/icatGeneralQuery.c
+++ iRODS/server/icat/src/icatGeneralQuery.c
@@ -1456,10 +1456,13 @@ generateSpecialQuery(genQueryInp_t genQueryInp, char *resultingSQL) {
    static char rescName[LONG_NAME_LEN];
    static char userName[NAME_LEN]="";
    static char userZone[NAME_LEN]="";
-   char quotaQuery1[]="select distinct QM.user_id, RM.resc_name, QM.quota_limit, QM.quota_over, QM.resc_id from R_QUOTA_MAIN QM, R_USER_MAIN UM, R_RESC_MAIN RM, R_USER_GROUP UG, R_USER_MAIN UM2 where ( (QM.user_id = UM.user_id and UM.user_name = ? and UM.zone_name = ?) or (QM.user_id = UG.group_user_id and UM2.user_name = ? and UM2.zone_name = ? and UG.user_id = UM2.user_id) ) and ((QM.resc_id = RM.resc_id) or QM.resc_id = '0') order by quota_over desc";
 
-   char quotaQuery2[]="select distinct QM.user_id, RM.resc_name, QM.quota_limit, QM.quota_over, QM.resc_id from R_QUOTA_MAIN QM, R_USER_MAIN UM, R_RESC_MAIN RM, R_USER_GROUP UG, R_USER_MAIN UM2 where ( (QM.user_id = UM.user_id and UM.user_name = ? and UM.zone_name=?) or (QM.user_id = UG.group_user_id and UM2.user_name = ? and UM2.zone_name = ? and UG.user_id = UM2.user_id) ) and ((QM.resc_id = RM.resc_id) or QM.resc_id = '0') and RM.resc_name = ? order by quota_over desc";
+   char quotaQuery1[]="( select distinct QM.user_id, RM.resc_name, QM.quota_limit, QM.quota_over, QM.resc_id from R_QUOTA_MAIN QM, R_RESC_MAIN RM, R_USER_GROUP UG, R_USER_MAIN UM2 where QM.resc_id = RM.resc_id AND (QM.user_id = UG.group_user_id and UM2.user_name = ? and UM2.zone_name = ? and UG.user_id = UM2.user_id )) UNION ( select distinct QM.user_id, RM.resc_name, QM.quota_limit, QM.quota_over, QM.resc_id from R_QUOTA_MAIN QM, R_USER_GROUP UG, R_USER_MAIN UM2, R_RESC_MAIN RM where QM.resc_id = '0' AND (QM.user_id = UG.group_user_id and UM2.user_name = ? and UM2.zone_name = ? and UG.user_id = UM2.user_id)) UNION ( select distinct QM.user_id, RM.resc_name, QM.quota_limit, QM.quota_over, QM.resc_id from R_QUOTA_MAIN QM, R_USER_MAIN UM, R_RESC_MAIN RM WHERE (QM.resc_id = RM.resc_id or QM.resc_id = '0') AND (QM.user_id = UM.user_id and UM.user_name = ? and UM.zone_name = ? )) order by quota_over DESC";
+
+  char quotaQuery2[]="( select distinct QM.user_id, RM.resc_name, QM.quota_limit, QM.quota_over, QM.resc_id from R_QUOTA_MAIN QM, R_RESC_MAIN RM, R_USER_GROUP UG, R_USER_MAIN UM2 where QM.resc_id = RM.resc_id AND RM.resc_name = ? AND (QM.user_id = UG.group_user_id and UM2.user_name = ? and UM2.zone_name = ? and UG.user_id = UM2.user_id )) UNION ( select distinct QM.user_id, RM.resc_name, QM.quota_limit, QM.quota_over, QM.resc_id from R_QUOTA_MAIN QM, R_USER_GROUP UG, R_USER_MAIN UM2, R_RESC_MAIN RM where QM.resc_id = '0' AND RM.resc_name = ? AND (QM.user_id = UG.group_user_id and UM2.user_name = ? and UM2.zone_name = ? and UG.user_id = UM2.user_id)) UNION ( select distinct QM.user_id, RM.resc_name, QM.quota_limit, QM.quota_over, QM.resc_id from R_QUOTA_MAIN QM, R_USER_MAIN UM, R_RESC_MAIN RM WHERE (QM.resc_id = RM.resc_id or QM.resc_id = '0') AND RM.resc_name = ? AND (QM.user_id = UM.user_id and UM.user_name = ? and UM.zone_name = ? )) order by quota_over DESC";
+
    int i, valid=0;
+   int cllCounter=cllBindVarCount;
 
    for (i=0; i<genQueryInp.sqlCondInp.len;i++) {
       if (genQueryInp.sqlCondInp.inx[i]==COL_USER_NAME) {
@@ -1472,13 +1475,16 @@ generateSpecialQuery(genQueryInp_t genQueryInp, char *resultingSQL) {
 	    strncpy(userZone, zoneName, sizeof userZone);
 	    rodsLog(LOG_ERROR,"userZone1=:%s:\n",userZone);
 	 }
-	 rodsLog(LOG_ERROR,"userZone2=:%s:\n",userZone);
-	 rodsLog(LOG_ERROR,"userName=:%s:\n",userName);
-	 rodsLog(LOG_ERROR,"in=:%s:\n",genQueryInp.sqlCondInp.value[i]);
-	 cllBindVars[cllBindVarCount++]=userName;
-	 cllBindVars[cllBindVarCount++]=userZone;
-	 cllBindVars[cllBindVarCount++]=userName;
-	 cllBindVars[cllBindVarCount++]=userZone;
+	 rodsLog(LOG_DEBUG,"spQuery(1) userZone2=:%s:\n",userZone);
+	 rodsLog(LOG_DEBUG,"spQuery(1) userName=:%s:\n",userName);
+	 rodsLog(LOG_DEBUG,"spQuery(1) in=:%s:\n",
+		 genQueryInp.sqlCondInp.value[i]);
+         cllBindVars[cllBindVarCount++]=userName;
+         cllBindVars[cllBindVarCount++]=userZone;
+         cllBindVars[cllBindVarCount++]=userName;
+         cllBindVars[cllBindVarCount++]=userZone;
+         cllBindVars[cllBindVarCount++]=userName;
+         cllBindVars[cllBindVarCount++]=userZone;
 	 strncpy(resultingSQL, quotaQuery1, MAX_SQL_SIZE_GQ);
 	 valid=1;
       }
@@ -1486,9 +1492,22 @@ generateSpecialQuery(genQueryInp_t genQueryInp, char *resultingSQL) {
    if (valid==0) return(CAT_INVALID_ARGUMENT);
    for (i=0; i<genQueryInp.sqlCondInp.len;i++) {
       if (genQueryInp.sqlCondInp.inx[i]==COL_R_RESC_NAME) {
+	 rodsLog(LOG_DEBUG,"spQuery(2) userZone2=:%s:\n",userZone);
+	 rodsLog(LOG_DEBUG,"spQuery(2) userName=:%s:\n",userName);
+	 rodsLog(LOG_DEBUG,"spQuery(2) in=:%s:\n",
+		 genQueryInp.sqlCondInp.value[i]);
 	 strncpy(rescName, genQueryInp.sqlCondInp.value[i], sizeof rescName);
-	 cllBindVars[cllBindVarCount++]=rescName;
-	 strncpy(resultingSQL, quotaQuery2, MAX_SQL_SIZE_GQ);
+         cllBindVars[cllCounter++]=rescName;
+         cllBindVars[cllCounter++]=userName;
+         cllBindVars[cllCounter++]=userZone;
+         cllBindVars[cllCounter++]=rescName;
+         cllBindVars[cllCounter++]=userName;
+         cllBindVars[cllCounter++]=userZone;
+         cllBindVars[cllCounter++]=rescName;
+         cllBindVars[cllCounter++]=userName;
+         cllBindVars[cllCounter++]=userZone;
+         strncpy(resultingSQL, quotaQuery2, MAX_SQL_SIZE_GQ);
+         cllBindVarCount=cllCounter;
       }
    }
    return (0);
