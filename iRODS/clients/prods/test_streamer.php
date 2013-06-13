931c800792cb72a99dec8dbe1fc6e9d9bba6dae8
michael-conway <michael.c.conway@gmail.com>
2013-06-13 14:21:24 +0000

    remove old PRODS and web contents (which were the PHP libraries) and inserted README files that point to the GForge/git versions
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@5485 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/clients/prods/test_streamer.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require_once("src/ProdsStreamer.class.php");
-  $str=file_get_contents("rods://rods.tempZone:RODS@rt.sdsc.edu:1247/tempZone/home/rods/test.sifang2.txt");
-  var_dump ($str);
-  
-  $children = scandir("rods://rods.tempZone:RODS@rt.sdsc.edu:1247/tempZone/home/rods");
-  var_dump ($children);
-?>
--- iRODS/clients/prods/tutorials/Prods/gettingstarted.php
+++
@@ -1,4 +0,0 @@
-<?php 
-  require_once("/Path/to/Prods/src/Prods.inc.php");
-  ... rest of your code ...
-?>
