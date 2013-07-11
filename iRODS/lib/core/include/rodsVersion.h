8345191700579127cf13d1178e26e6dab62dc7ef
Wayne-Schroeder <schroeder@diceresearch.org>
2013-07-11 20:31:52 +0000

    Update the version and release date for releasing 3.3 next week.
    git-svn-id: svn://irodssvn.ucsd.edu/trunk@5505 ea137bd6-aa86-47b6-b90d-b101d9f19a90

--- iRODS/lib/core/include/rodsVersion.h
+++ iRODS/lib/core/include/rodsVersion.h
@@ -9,9 +9,9 @@
 #ifndef RODS_VERSION_H
 #define RODS_VERSION_H
 
-#define RODS_REL_VERSION	"rods3.2"
+#define RODS_REL_VERSION	"rods3.3"
 #define RODS_API_VERSION	"d"
-#define RODS_RELEASE_DATE       "October 2012"
+#define RODS_RELEASE_DATE       "July 2013"
 
 
 #endif	/* RODS_VERSION_H */
